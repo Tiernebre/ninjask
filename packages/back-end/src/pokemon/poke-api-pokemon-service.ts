3f44fab66230cdcb9dc48ea13b5d08688e484795
Brendan Tierney <tiernebre@gmail.com>
2021-04-13 20:00:58 -0600

    resource list of pokemon

--- packages/back-end/src/pokemon/poke-api-pokemon-service.ts
+++ packages/back-end/src/pokemon/poke-api-pokemon-service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from "../http/http-client";
+import { NamedAPIResourceList } from "../poke-api/named-api-resource-list";
 import { Pokemon } from "./pokemon";
 import { PokemonService } from "./pokemon-service";
 
@@ -6,6 +7,10 @@ export class PokeApiPokemonService implements PokemonService {
   constructor(
     private readonly pokeApiHttpClient: HttpClient
   ) {}
+
+  public async getAll(): Promise<NamedAPIResourceList> {
+    return this.pokeApiHttpClient.get('pokemon')
+  }
 
   public async getOne(id: number): Promise<Pokemon> {
     return this.pokeApiHttpClient.get(`pokemon/${id}`)

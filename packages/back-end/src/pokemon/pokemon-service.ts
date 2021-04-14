1be1ef3f065611f5edd39d580623bc41c1232e87
Brendan Tierney <tiernebre@gmail.com>
2021-04-13 19:55:17 -0600

    pokemon service interface

--- packages/back-end/src/pokemon/pokemon-service.ts
+++ packages/back-end/src/pokemon/pokemon-service.ts
@@ -1,3 +1,9 @@
+import { Pokemon } from "./pokemon";
+
 export interface PokemonService {
-  getOne(id: number)
+  /**
+   * Returns a Pokemon from a given pokemon id.
+   * @param id The id of the Pokemon to get.
+   */
+  getOne(id: number): Promise<Pokemon>
 }

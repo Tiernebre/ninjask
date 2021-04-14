945701f4977dc01dc48e8e02833a2733bfa389dd
Brendan Tierney <tiernebre@gmail.com>
2021-04-13 21:52:31 -0600

    unit test setup

--- packages/back-end/src/random/random.spec.ts
+++ packages/back-end/src/random/random.spec.ts
@@ -1,10 +1,18 @@
-import { getRandomArbitrary } from './random'
+import { getRandomInt } from './random'
 
 describe('random', () => {
-  describe('getRandomArbitrary', () => {
+  describe('getRandomInt', () => {
     it('returns an integer', () => {
-      const retval = getRandomArbitrary(1, 10)
+      const retval = getRandomInt(1, 10)
       expect(typeof retval).toEqual('number')
+    })
+
+    it('returns properly calculated number given a fixed random seed', () => {
+      const mockMath: Math = Object.create(global.Math) as Math;
+      mockMath.random = () => 0.5;
+      global.Math = mockMath;
+      const retval = getRandomInt(1, 10)
+      expect(retval).toEqual(5)
     })
   })
 })
--- packages/back-end/src/random/random.ts
+++ packages/back-end/src/random/random.ts
@@ -1,3 +1,5 @@
-export const getRandomArbitrary = (min: number, max: number) => {
-  return Math.random() * (max - min) + min;
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }

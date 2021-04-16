64877c65a5bdb3437041ce9cb9a3dce020d9026a
Brendan Tierney <tiernebre@gmail.com>
2021-04-15 21:01:17 -0600

    allow cors for any origin

--- packages/back-end/src/app.ts
+++ packages/back-end/src/app.ts
@@ -7,6 +7,7 @@ import Router from "@koa/router";
 import { PokemonRouter } from "./pokemon/pokemon.router";
 import { Logger, PinoLogger } from "./logger";
 import { loggingMiddleware } from "./logger/logging.middleware";
+import cors from '@koa/cors';
 
 const app = new Koa();
 
@@ -23,6 +24,10 @@ const pokemonService: PokemonService = new PokeApiPokemonService(
 
 const pokemonRouter: Router = new PokemonRouter(pokemonService);
 
+app.use(cors({
+  // not great CORS API design -- but we'll stick with it for now to get something we can iterate on. :)
+  origin: '*'
+}))
 app.use(loggingMiddleware(logger));
 app.use(pokemonRouter.routes());
 

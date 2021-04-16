a5c5c3bc87c02d295204354a8ef9825f7c191f2c
Brendan Tierney <tiernebre@gmail.com>
2021-04-15 19:54:46 -0600

    setup pino logging

--- packages/back-end/src/logger/pino-logger.ts
+++ packages/back-end/src/logger/pino-logger.ts
@@ -1,7 +1,15 @@
 import { Logger } from "./logger";
+import Pino from 'pino'
 
 export class PinoLogger implements Logger {
+  pino: Pino.Logger
+
+  constructor(
+  ) {
+    this.pino = Pino()
+  }
+
   info(message: string): void {
-    throw new Error("Method not implemented.");
+    this.pino.info(message)
   }
 }

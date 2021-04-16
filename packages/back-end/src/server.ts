78d18934aba05f483e519c99670d7157485e655d
Brendan Tierney <tiernebre@gmail.com>
2021-04-15 18:50:07 -0600

    endpoint for a random pokemon

--- packages/back-end/src/server.ts
+++ packages/back-end/src/server.ts
@@ -3,8 +3,10 @@ import { FetchHttpClient } from "./http/fetch-http-client";
 import { HttpClient } from "./http/http-client";
 import { PokeApiPokemonService } from "./pokemon/poke-api-pokemon-service";
 import { PokemonService } from "./pokemon/pokemon-service";
+import Router from '@koa/router';
 
 const app = new Koa();
+const router = new Router()
 
 const pokeApiHttpClient: HttpClient = new FetchHttpClient(
   "https://pokeapi.co/api/v2"
@@ -14,11 +16,11 @@ const pokemonService: PokemonService = new PokeApiPokemonService(
   pokeApiHttpClient
 );
 
-// response
-app.use(async (ctx) => {
-  ctx.body = "Hello Koa UPDATED AGAIN hopefully it works";
-  const testResponse = await pokemonService.getARandomOne();
-  ctx.body = JSON.stringify(testResponse);
-});
+router.get('/random-pokemon', async (ctx) => {
+  const randomPokemon = await pokemonService.getARandomOne();
+  ctx.body = randomPokemon
+})
+
+app.use(router.routes())
 
 export { app };

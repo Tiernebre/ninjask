39a1df7af27c26b24a27775a6b81b81fcd60f1a1
Brendan Tierney <tiernebre@gmail.com>
2021-04-16 19:07:18 -0600

    working connection with repositories

--- packages/back-end/src/pokemon/pokemon.entity.ts
+++ packages/back-end/src/pokemon/pokemon.entity.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'pokemon' })
 export class PokemonEntity {
   @PrimaryGeneratedColumn()
   public id!: number;
+
+  @Column()
+  public name!: string;
 }

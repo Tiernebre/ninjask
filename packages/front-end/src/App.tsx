957a1ea5fe966a231fb454ecf2340d14d2dbe473
Brendan Tierney <tiernebre@gmail.com>
2021-04-18 15:29:07 -0600

    working setup for pokemon!

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -1,33 +1,38 @@
 import "./App.css";
 import { useCallback } from 'react';
-import useWebSocket from 'react-use-websocket';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 function App() {
-  const { sendMessage, lastMessage } = useWebSocket('ws://localhost:3000/live-draft')
-
+  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://localhost:3000/live-draft')
   const restartDraft = useCallback(() => sendMessage('RESTART'), [])
+  const fetchRequest = useCallback(() => sendMessage('NEXT'), [])
 
-  const fetchRequest = useCallback(() => {
-    async function fetchPokemon() {
-      sendMessage('NEXT')
+  const isReady = () => readyState === ReadyState.OPEN
+
+  console.log(lastMessage)
+  const pokemon = () => {
+    try {
+      return isReady() && lastMessage ? JSON.parse(lastMessage.data) : null
+    } catch (error) {
+      return null
     }
-    fetchPokemon()
-  }, [])
+  }
 
-  const pokemon = lastMessage ? JSON.parse(lastMessage.data) : null
-
-  const pokemonInformation = pokemon ? <div className="pokemon-information">
-    <img src={pokemon.imageUrl} alt={`${pokemon.name}`}></img>
-    <p>{ pokemon.name }</p>
+  const pokemonInformation = pokemon() ? <div className="pokemon-information">
+    <img src={pokemon().imageUrl} alt={`${pokemon().name}`}></img>
+    <p>{ pokemon().name }</p>
   </div> : <div></div>
+
+  const buttons = isReady() ? 
+      <div className="pokemon-draft-buttons">
+        <button onClick={restartDraft}>Restart the Draft!</button>
+        <button onClick={fetchRequest}>See the next available Pokemon!</button>
+      </div> : <div></div>
 
   return (
     <div className="app">
       {pokemonInformation}
-      <div className="pokemon-draft-buttons">
-        <button onClick={restartDraft}>Restart the Draft!</button>
-        <button onClick={fetchRequest}>See the next available Pokemon!</button>
-      </div>
+      {buttons}
     </div>
   )
 }

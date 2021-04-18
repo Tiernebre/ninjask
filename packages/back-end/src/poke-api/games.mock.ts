81872d34cee1109b8d2c298116f581b160779394
Brendan Tierney <tiernebre@gmail.com>
2021-04-17 23:13:13 -0600

    tests for getting pokedex of a version

--- packages/back-end/src/poke-api/games.mock.ts
+++ packages/back-end/src/poke-api/games.mock.ts
@@ -1,4 +1,4 @@
-import { PokeApiVersion } from "./games";
+import { PokeApiPokedex, PokeApiVersion, PokeApiVersionGroup } from "./games";
 
 export const generateMockPokeApiVersion = (): PokeApiVersion => ({
   id: 1,
@@ -58,4 +58,135 @@ export const generateMockPokeApiVersion = (): PokeApiVersion => ({
     name: "red-blue",
     url: "https://pokeapi.co/api/v2/version-group/1/",
   },
+});
+
+export const generateMockPokeApiVersionGroup = (): PokeApiVersionGroup => ({
+  generation: {
+    name: "generation-i",
+    url: "https://pokeapi.co/api/v2/generation/1/",
+  },
+  id: 1,
+  move_learn_methods: [
+    {
+      name: "level-up",
+      url: "https://pokeapi.co/api/v2/move-learn-method/1/",
+    },
+    {
+      name: "machine",
+      url: "https://pokeapi.co/api/v2/move-learn-method/4/",
+    },
+    {
+      name: "stadium-surfing-pikachu",
+      url: "https://pokeapi.co/api/v2/move-learn-method/5/",
+    },
+  ],
+  name: "red-blue",
+  order: 1,
+  pokedexes: [
+    {
+      name: "kanto",
+      url: "https://pokeapi.co/api/v2/pokedex/2/",
+    },
+  ],
+  regions: [
+    {
+      name: "kanto",
+      url: "https://pokeapi.co/api/v2/region/1/",
+    },
+  ],
+  versions: [
+    {
+      name: "red",
+      url: "https://pokeapi.co/api/v2/version/1/",
+    },
+    {
+      name: "blue",
+      url: "https://pokeapi.co/api/v2/version/2/",
+    },
+  ],
+});
+
+export const generateMockPokeApiPokedex = (): PokeApiPokedex => ({
+  descriptions: [
+    {
+      description: "Pokédex régional de Kanto dans Rouge/Bleu/Jaune",
+      language: {
+        name: "fr",
+        url: "https://pokeapi.co/api/v2/language/5/",
+      },
+    },
+    {
+      description: "Rot/Blau/Gelb Kanto Dex",
+      language: {
+        name: "de",
+        url: "https://pokeapi.co/api/v2/language/6/",
+      },
+    },
+    {
+      description: "Pokédex regional de Rojo/Azul/Amarillo",
+      language: {
+        name: "es",
+        url: "https://pokeapi.co/api/v2/language/7/",
+      },
+    },
+    {
+      description: "Red/Blue/Yellow Kanto dex",
+      language: {
+        name: "en",
+        url: "https://pokeapi.co/api/v2/language/9/",
+      },
+    },
+  ],
+  id: 2,
+  is_main_series: true,
+  name: "kanto",
+  names: [
+    {
+      language: {
+        name: "fr",
+        url: "https://pokeapi.co/api/v2/language/5/",
+      },
+      name: "Kanto",
+    },
+    {
+      language: {
+        name: "de",
+        url: "https://pokeapi.co/api/v2/language/6/",
+      },
+      name: "Kanto",
+    },
+    {
+      language: {
+        name: "es",
+        url: "https://pokeapi.co/api/v2/language/7/",
+      },
+      name: "Kanto",
+    },
+    {
+      language: {
+        name: "en",
+        url: "https://pokeapi.co/api/v2/language/9/",
+      },
+      name: "Kanto",
+    },
+  ],
+  pokemon_entries: [],
+  region: {
+    name: "kanto",
+    url: "https://pokeapi.co/api/v2/region/1/",
+  },
+  version_groups: [
+    {
+      name: "red-blue",
+      url: "https://pokeapi.co/api/v2/version-group/1/",
+    },
+    {
+      name: "yellow",
+      url: "https://pokeapi.co/api/v2/version-group/2/",
+    },
+    {
+      name: "firered-leafgreen",
+      url: "https://pokeapi.co/api/v2/version-group/7/",
+    },
+  ],
 });

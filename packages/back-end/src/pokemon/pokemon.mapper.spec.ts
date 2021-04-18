5544c39378cdf01e812f15487c786384920d1f92
Brendan Tierney <tiernebre@gmail.com>
2021-04-18 16:07:43 -0600

    include icon map for pokemon dto

--- packages/back-end/src/pokemon/pokemon.mapper.spec.ts
+++ packages/back-end/src/pokemon/pokemon.mapper.spec.ts
@@ -6,12 +6,16 @@ describe("pokemon.mapper", () => {
     it("returns a properly formatted pokemon from its PokeAPI version", () => {
       const pokeApiPokemon = generateMockPokeApiPokemonSpecies();
       pokeApiPokemon.id = 1;
+      pokeApiPokemon.name = 'pikachu'
       const pokemonCreated = mapFromPokeApi(pokeApiPokemon);
       expect(pokemonCreated.id).toEqual(pokeApiPokemon.id);
       expect(pokemonCreated.name).toEqual(pokeApiPokemon.name);
       expect(pokemonCreated.imageUrl).toEqual(
         "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
       );
+      expect(pokemonCreated.iconUrl).toEqual(
+        "https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/pikachu.png"
+      )
     });
 
     it("returns a properly formatted image URL for a pokemon with an id in the double digits", () => {
--- packages/back-end/src/pokemon/pokemon.mapper.ts
+++ packages/back-end/src/pokemon/pokemon.mapper.ts
@@ -3,6 +3,8 @@ import { Pokemon } from "./pokemon";
 
 const POKEMON_IMAGE_URL =
   "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images";
+const POKEMON_ICON_IMAGE_URL = 
+  "https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular"
 
 const getImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
   const id = pokemon.id.toString();
@@ -10,12 +12,17 @@ const getImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
   return `${POKEMON_IMAGE_URL}/${paddedId}.png`;
 };
 
+const getIconImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
+  return `${POKEMON_ICON_IMAGE_URL}/${pokemon.name.toLowerCase()}.png`;
+}
+
 export const mapFromPokeApi = (
   pokeApiPokemon: PokeApiPokemonSpecies
 ): Pokemon => {
   return new Pokemon(
     pokeApiPokemon.id,
     pokeApiPokemon.name,
-    getImageUrlForPokemon(pokeApiPokemon)
+    getImageUrlForPokemon(pokeApiPokemon),
+    getIconImageUrlForPokemon(pokeApiPokemon)
   );
 };

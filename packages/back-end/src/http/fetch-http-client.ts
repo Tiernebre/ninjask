c2249ca0d28fc205a28cca5f87c567b9d3a418f2
Brendan Tierney <tiernebre@gmail.com>
2021-04-17 20:36:03 -0600

    consolidate fetch into its own utility function

--- packages/back-end/src/http/fetch-http-client.ts
+++ packages/back-end/src/http/fetch-http-client.ts
@@ -1,16 +1,10 @@
 import { HttpClient } from "./http-client";
-import fetch from "node-fetch";
-import { HttpError } from "./http-error";
+import { fetchOk } from "./utilities";
 
 export class FetchHttpClient implements HttpClient {
   constructor(private readonly url: string) {}
 
   async get<T>(uri: string): Promise<T> {
-    const response = await fetch(`${this.url}/${uri}`);
-    if (response.ok) {
-      return response.json() as Promise<T>;
-    } else {
-      throw new HttpError();
-    }
+    return fetchOk<T>(`${this.url}/${uri}`);
   }
 }

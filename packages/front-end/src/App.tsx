0ab5499ebcc1e6f4fcabf6cb3c9c86b8c0d1f051
Brendan Tierney <tiernebre@gmail.com>
2021-04-18 18:50:18 -0600

    fix url (#34)

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -12,7 +12,7 @@ interface DraftStatus {
 }
 
 function App() {
-  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://localhost:3000/live-draft')
+  const { sendMessage, lastMessage, readyState } = useWebSocket('ws://ec2-35-163-100-24.us-west-2.compute.amazonaws.com:3000/live-draft')
   const restartDraft = useCallback(() => sendMessage('RESTART'), [])
   const fetchRequest = useCallback(() => sendMessage('NEXT'), [])
 

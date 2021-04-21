21f8fed0d666d20179eab545e5f47d46d999639f
Brendan Tierney <tiernebre@gmail.com>
2021-04-20 20:34:02 -0600

    bcrypt encoder tests

---
+++ packages/back-end/src/crypto/bcrypt-password-encoder.spec.ts
@@ -0,0 +1,17 @@
+import { BCryptPasswordEncoder } from './bcrypt-password-encoder'
+
+describe('bcrypt-password-encoder', () => {
+  let bcryptPasswordEncoder: BCryptPasswordEncoder
+
+  beforeEach(() => {
+    bcryptPasswordEncoder = new BCryptPasswordEncoder()
+  })
+
+  describe('encode', () => {
+    it('returns an encoded password that is different from the raw password provided', async () => {
+      const rawPassword = 'someTestPassword456!'
+      const encodedPassword = await bcryptPasswordEncoder.encode(rawPassword)
+      expect(encodedPassword).not.toEqual(rawPassword)
+    })
+  })
+})

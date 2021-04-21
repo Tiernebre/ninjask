03e56fda40b1e460dd4dcac65b96e3f656c1577f
Brendan Tierney <tiernebre@gmail.com>
2021-04-20 21:46:29 -0600

    initial session middleware setup

---
+++ packages/back-end/src/session/session.middleware.ts
@@ -0,0 +1,19 @@
+import { Context, Next } from "koa";
+import { SessionService } from "./session.service";
+import { UNAUTHORIZED, FORBIDDEN } from 'http-status'
+
+export const sessionMiddleware = (sessionService: SessionService) => (ctx: Context, next: Next): void => {
+  if (!ctx.header.authorization) {
+    ctx.status = UNAUTHORIZED
+    throw new Error('Authentication must be provided.')
+  }
+
+  try {
+    const user = sessionService.verifyOne(ctx.header.authorization)
+    ctx.state.user = user
+    void next()
+  } catch (error) {
+    ctx.status = FORBIDDEN
+    throw new Error('Authentication provided was invalid.')
+  }
+}

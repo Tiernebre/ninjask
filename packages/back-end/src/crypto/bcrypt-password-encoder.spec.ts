161f184831ae1535ea8cd121955305ee27b980f1
Brendan Tierney <tiernebre@gmail.com>
2021-04-20 20:36:36 -0600

    working tests for compare

--- packages/back-end/src/crypto/bcrypt-password-encoder.spec.ts
+++ packages/back-end/src/crypto/bcrypt-password-encoder.spec.ts
@@ -1,4 +1,5 @@
 import { BCryptPasswordEncoder } from "./bcrypt-password-encoder";
+import bcrypt from 'bcrypt'
 
 describe("bcrypt-password-encoder", () => {
   let bcryptPasswordEncoder: BCryptPasswordEncoder;
@@ -14,4 +15,18 @@ describe("bcrypt-password-encoder", () => {
       expect(encodedPassword).not.toEqual(rawPassword);
     });
   });
+
+  describe("compare", () => {
+    it('returns true if the raw password matches the encoded stored password', async () => {
+      const rawPassword = 'someTestPassword123!'
+      const encodedPassword = await bcrypt.hash(rawPassword, 12)
+      await expect(bcryptPasswordEncoder.matches(rawPassword, encodedPassword)).resolves.toEqual(true)
+    })
+
+    it('returns false if the raw password does not match the encoded stored password', async () => {
+      const rawPassword = 'someTestPassword123!'
+      const encodedPassword = await bcrypt.hash(rawPassword + 'actually NOT', 12)
+      await expect(bcryptPasswordEncoder.matches(rawPassword, encodedPassword)).resolves.toEqual(false)
+    })
+  })
 });

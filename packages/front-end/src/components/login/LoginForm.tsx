cff8112beeb9505d07ca069bfd35ff6f7baa55ec
Brendan Tierney <tiernebre@gmail.com>
2021-04-22 20:04:10 -0600

    login form with ref register

--- packages/front-end/src/components/login/LoginForm.tsx
+++ packages/front-end/src/components/login/LoginForm.tsx
@@ -1,10 +1,22 @@
+import { useForm } from "react-hook-form";
+
 export const LoginForm = () => {
+  const { register } = useForm();
+
   return (
     <form className="LoginForm">
       <label htmlFor="LoginForm__access-key">Access Key</label>
-      <input type="text" id="LoginForm__access-key" name="accessKey" />
+      <input
+        type="text"
+        id="LoginForm__access-key"
+        {...register("accessKey", { required: true })}
+      />
       <label htmlFor="LoginForm__password">Password</label>
-      <input type="password" id="LoginForm__password" name="password" />
+      <input
+        type="password"
+        id="LoginForm__password"
+        {...register("password", { required: true })}
+      />
     </form>
   );
 };

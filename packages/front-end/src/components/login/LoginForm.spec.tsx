c78de6cf26489d39afb79dc1e57a0bf8e9300cfc
Brendan Tierney <tiernebre@gmail.com>
2021-04-22 21:05:08 -0600

    way easier tests

--- packages/front-end/src/components/login/LoginForm.spec.tsx
+++ packages/front-end/src/components/login/LoginForm.spec.tsx
@@ -5,23 +5,25 @@ import { act } from "react-dom/test-utils";
 
 const getAccessKeyInput = () => screen.getByLabelText(/Access Key/i)
 const getPasswordInput = () => screen.getByLabelText(/Password/i)
+const getSubmitButton = () => screen.getByRole('button', { name: /Login/i })
+const getErrorMessage = () => screen.getByRole('alert', { name: /This field is required/i })
 
 it('renders an error message and marks the access key input invalid if it is not filled out', async () => {
   render(<LoginForm />)
   await act(async () => {
     await user.type(getPasswordInput(), 'p@55w0rd')
-    await user.click(screen.getByRole('button', { name: /Login/i }))
+    await user.click(getSubmitButton())
   })
   expect(getAccessKeyInput()).toBeInvalid()
-  expect(screen.getByRole('alert', { name: /This field is required/i })).toBeInTheDocument()
+  expect(getErrorMessage()).toBeInTheDocument()
 })
 
 it('renders an error message and marks the password input invalid if it is not filled out', async () => {
   render(<LoginForm />)
   await act(async () => {
     await user.type(getAccessKeyInput(), 'some-access-key-value')
-    await user.click(screen.getByRole('button', { name: /Login/i }))
+    await user.click(getSubmitButton())
   })
   expect(getPasswordInput()).toBeInvalid()
-  expect(screen.getByRole('alert', { name: /This field is required/i })).toBeInTheDocument()
+  expect(getErrorMessage()).toBeInTheDocument()
 })

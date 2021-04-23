ee76cfa4cc5ff183f655c1f912af8dd0cb0d3065
Brendan Tierney <tiernebre@gmail.com>
2021-04-22 20:19:53 -0600

    very basic validation

--- packages/front-end/src/components/login/LoginForm.tsx
+++ packages/front-end/src/components/login/LoginForm.tsx
@@ -1,22 +1,31 @@
 import { useForm } from "react-hook-form";
 
+type LoginFormData = {
+  accessKey: string,
+  password: string
+}
+
 export const LoginForm = () => {
-  const { register } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginFormData>();
+  const onSubmit = handleSubmit(data => console.log(data))
 
   return (
-    <form className="LoginForm">
+    <form className="LoginForm" onSubmit={onSubmit}>
       <label htmlFor="LoginForm__access-key">Access Key</label>
       <input
         type="text"
         id="LoginForm__access-key"
         {...register("accessKey", { required: true })}
       />
+      {errors.accessKey && <span>This field is required</span>}
       <label htmlFor="LoginForm__password">Password</label>
       <input
         type="password"
         id="LoginForm__password"
         {...register("password", { required: true })}
       />
+      {errors.password && <span>This field is required</span>}
+      <button>Login</button>
     </form>
   );
 };

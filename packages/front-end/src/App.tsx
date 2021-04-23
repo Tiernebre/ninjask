0e140d2f2b029b10178a261e05297ac5c8e0cf2e
Brendan Tierney <tiernebre@gmail.com>
2021-04-22 19:35:15 -0600

    working login route

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -1,10 +1,18 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Login } from "./views/Login";
 
 function App() {
   return (
     <div className="App">
       <p>Welcome to Ninjask!</p>
+      <Router>
+        <Switch>
+          <Route path={["/", "/login"]}>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

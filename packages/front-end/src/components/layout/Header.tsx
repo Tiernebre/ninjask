b0dc346cf39d7b1831089be73161bbf8c9e2c314
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 14:28:04 -0600

    responsive logout button

--- packages/front-end/src/components/layout/Header.tsx
+++ packages/front-end/src/components/layout/Header.tsx
@@ -1,9 +1,9 @@
-import "./Header.css";
+import "./Header.scss";
 
 export const Header = () => (
   <header className="Header columns is-vcentered is-mobile">
-    <div className="control column is-offset-10-desktop is-offset-9-tablet">
-      {/* <NightModeToggle /> */}
+    <div className="control column is-offset-11-desktop is-offset-10-tablet">
+      <button className="Header__log-out-button button is-danger">Log Out</button>
     </div>
   </header>
 );

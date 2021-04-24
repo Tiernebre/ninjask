6a0970a660e77001e60150de6827ed79b25ab841
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 00:22:19 -0600

    more responsive

--- packages/front-end/src/components/layout/Header.tsx
+++ packages/front-end/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 
 export const Header = () => (
   <header className="Header columns is-vcentered is-mobile">
-    <div className="control column is-offset-11">
+    <div className="control column is-offset-10-desktop is-offset-9-tablet">
       <NightModeToggle />
     </div>
   </header>

59c3b3d7047be60d480b1fd625ccbe0171cc49ac
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 14:53:38 -0600

    loading message test

---
+++ packages/front-end/src/components/session/SessionRefresher.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import { object } from 'testdouble'
+import { SessionService } from '../../api/session'
+import { SessionRefresher } from './SessionRefresher'
+
+it('displays a loading message while a refresh occurs', () => {
+  const sessionService = object<SessionService>()
+  sessionService.refreshCurrentSession = () => new Promise(res => setTimeout(res, 5000))
+  render(
+    <SessionRefresher 
+      onSessionRefresh={jest.fn()}
+      onSessionRefreshFail={jest.fn()}
+      sessionService={sessionService}
+    >
+      <p>Session has refreshed</p>
+    </SessionRefresher>
+  )
+  expect(screen.getByText('Loading...')).toBeInTheDocument()
+})

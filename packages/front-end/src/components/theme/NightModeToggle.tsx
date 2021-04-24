e0b212a69e1e50c1018da0d720d3b4a8d7b0a7d8
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 00:03:00 -0600

    persisted night mode setup :)

--- packages/front-end/src/components/theme/NightModeToggle.tsx
+++ packages/front-end/src/components/theme/NightModeToggle.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
 import Switch from 'react-switch'
+import useLocalStorage from 'react-use-localstorage'
+
+const NIGHT_MODE_KEY = 'night-mode'
 
 export const NightModeToggle = () =>{
-  const [nightModeEnabled, setNightModeEnabled] = useState(false)
+  const [nightModeEnabled, setNightModeEnabled] = useLocalStorage(NIGHT_MODE_KEY)
+  
+  const updateNightMode = (nightModeEnabled: boolean) => {
+    setNightModeEnabled(nightModeEnabled.toString())
+  }
 
-  return <Switch checked={nightModeEnabled} onChange={setNightModeEnabled} />
+  return <Switch checked={nightModeEnabled === 'true'} onChange={updateNightMode} />
 } 

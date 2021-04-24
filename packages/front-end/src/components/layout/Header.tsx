dd63139b40a26e2c8b51ed7426e3b02973891fd8
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 00:33:53 -0600

    Setup Night Theme Toggle (#58)
    * night mode setup!
    
    * persisted night mode setup :)
    
    * working night theme toggle!
    
    * more accessible night mode switch
    
    * more responsive
    
    * night mode toggle tests
    
    * clean up formatting

---
+++ packages/front-end/src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import { NightModeToggle } from "../theme/NightModeToggle";
+import "./Header.css";
+
+export const Header = () => (
+  <header className="Header columns is-vcentered is-mobile">
+    <div className="control column is-offset-10-desktop is-offset-9-tablet">
+      <NightModeToggle />
+    </div>
+  </header>
+);

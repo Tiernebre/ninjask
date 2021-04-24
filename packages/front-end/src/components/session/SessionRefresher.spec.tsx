4e1036ff490b2200a393ecb5711e2d17c442e415
Brendan Tierney <tiernebre@gmail.com>
2021-04-24 14:56:28 -0600

    session refresher tests

--- packages/front-end/src/components/session/SessionRefresher.spec.tsx
+++ packages/front-end/src/components/session/SessionRefresher.spec.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import { object } from "testdouble";
+import flushPromises from "flush-promises";
+import { act } from "react-dom/test-utils";
+import { object, when } from "testdouble";
 import { SessionService } from "../../api/session";
 import { SessionRefresher } from "./SessionRefresher";
+
+const loadingMessage = 'Loading...'
+const childrenMessage = 'Session has refreshed'
 
 it("displays a loading message while a refresh occurs", () => {
   const sessionService = object<SessionService>();
@@ -13,8 +18,28 @@ it("displays a loading message while a refresh occurs", () => {
       onSessionRefreshFail={jest.fn()}
       sessionService={sessionService}
     >
-      <p>Session has refreshed</p>
+      <p>{childrenMessage}</p>
     </SessionRefresher>
   );
-  expect(screen.getByText("Loading...")).toBeInTheDocument();
+  expect(screen.getByText(loadingMessage)).toBeInTheDocument();
+  expect(screen.queryByText(childrenMessage)).toBeNull()
+});
+
+it("displays given children components when a refresh completed", async () => {
+  const sessionService = object<SessionService>();
+  when(sessionService.refreshCurrentSession()).thenResolve({ accessToken: 'some-valid-access-token' })
+  render(
+    <SessionRefresher
+      onSessionRefresh={jest.fn()}
+      onSessionRefreshFail={jest.fn()}
+      sessionService={sessionService}
+    >
+      <p>{childrenMessage}</p>
+    </SessionRefresher>
+  );
+  await act(async () => {
+    await flushPromises()
+  })
+  expect(screen.getByText(childrenMessage)).toBeInTheDocument()
+  expect(screen.queryByText(loadingMessage)).toBeNull()
 });

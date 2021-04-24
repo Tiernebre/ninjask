ae7dccf02025514894d7f43cfc638818eadb84e7
Brendan Tierney <tiernebre@gmail.com>
2021-04-23 23:30:45 -0600

    more code formatting!!!

--- packages/back-end/src/environment.spec.ts
+++ packages/back-end/src/environment.spec.ts
@@ -1,23 +1,29 @@
-import { isProduction } from './environment'
+import { isProduction } from "./environment";
 
-describe('environment', () => {
+describe("environment", () => {
   beforeEach(() => {
-    delete process.env.NODE_ENV
-  })
+    delete process.env.NODE_ENV;
+  });
 
-  describe('isProduction', () => {
-    it('returns false if NODE_ENV is not set', () => {
-      expect(isProduction()).toEqual(false)
-    })
+  describe("isProduction", () => {
+    it("returns false if NODE_ENV is not set", () => {
+      expect(isProduction()).toEqual(false);
+    });
 
-    it.each(['', 'development', 'dev', 'test', 'TEST', 'DEV'])('returns false if NODE_ENV equals %p', (env: string) => {
-      process.env.NODE_ENV = env
-      expect(isProduction()).toEqual(false)
-    })
+    it.each(["", "development", "dev", "test", "TEST", "DEV"])(
+      "returns false if NODE_ENV equals %p",
+      (env: string) => {
+        process.env.NODE_ENV = env;
+        expect(isProduction()).toEqual(false);
+      }
+    );
 
-    it.each(['production', 'Production', 'PRODUCTION'])('returns true if NODE_ENV equals %p', (env: string) => {
-      process.env.NODE_ENV = env
-      expect(isProduction()).toEqual(true)
-    })
-  })
-})
+    it.each(["production", "Production", "PRODUCTION"])(
+      "returns true if NODE_ENV equals %p",
+      (env: string) => {
+        process.env.NODE_ENV = env;
+        expect(isProduction()).toEqual(true);
+      }
+    );
+  });
+});
--- packages/back-end/src/environment.ts
+++ packages/back-end/src/environment.ts
@@ -1,3 +1,5 @@
 const PRODUCTION_NAME = "production";
 
-export const isProduction = (): boolean => !!process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === PRODUCTION_NAME.toLowerCase();
+export const isProduction = (): boolean =>
+  !!process.env.NODE_ENV &&
+  process.env.NODE_ENV.toLowerCase() === PRODUCTION_NAME.toLowerCase();

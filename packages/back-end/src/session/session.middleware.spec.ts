61f078e6453bbd5f761c77f398f18955ea3d42ff
Brendan Tierney <tiernebre@gmail.com>
2021-04-25 17:31:45 -0600

    working session middleware test

--- packages/back-end/src/session/session.middleware.spec.ts
+++ packages/back-end/src/session/session.middleware.spec.ts
@@ -1,8 +1,10 @@
 import { sessionMiddleware } from './session.middleware'
-import { object } from 'testdouble'
+import { object, when } from 'testdouble'
 import { SessionService } from './session.service'
 import { Context, Next } from 'koa'
-import { UNAUTHORIZED } from 'http-status'
+import { FORBIDDEN, OK, UNAUTHORIZED } from 'http-status'
+import { generateRandomNumber, generateRandomString } from '../random'
+import { SessionPayload } from './session-payload'
 
 describe('sessionMiddleware', () => {
   let sessionService: SessionService
@@ -21,5 +23,39 @@ describe('sessionMiddleware', () => {
     }
     await expect(sessionMiddlewareToTest(mockCtx, mockNext)).rejects.toThrowError()
     expect(mockCtx.status).toEqual(UNAUTHORIZED)
+    expect(mockNext).not.toHaveBeenCalled()
+  })
+
+  it("throws an error if the access token provided is invalid", async () => {
+    const mockCtx = object<Context>()
+    const mockNext = jest.fn()
+    const authorization = 'some-invalid-token'
+    mockCtx.header = {
+      authorization
+    }
+    when(sessionService.verifyOne(authorization)).thenThrow(new Error())
+    await expect(sessionMiddlewareToTest(mockCtx, mockNext)).rejects.toThrowError()
+    expect(mockCtx.status).toEqual(FORBIDDEN)
+    expect(mockNext).not.toHaveBeenCalled()
+  })
+
+  it("resumes and goes through the middleware if the access token provided is valid", async () => {
+    const mockCtx = object<Context>()
+    const mockNext = jest.fn()
+    const authorization = 'some-invalid-token'
+    mockCtx.status = OK
+    mockCtx.header = {
+      authorization
+    }
+    mockCtx.state = {}
+    const sessionPayload: SessionPayload = {
+      id: generateRandomNumber(),
+      accessKey: generateRandomString()
+    }
+    when(sessionService.verifyOne(authorization)).thenReturn(sessionPayload)
+    await expect(sessionMiddlewareToTest(mockCtx, mockNext)).resolves.not.toThrowError()
+    expect(mockCtx.status).toEqual(OK)
+    expect(mockNext).toHaveBeenCalled()
+    expect(mockCtx.state.user).toEqual(sessionPayload)
   })
 })

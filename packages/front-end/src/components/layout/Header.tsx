48901a07986d36198746a2bfdc5bb64ac95d33ad
Brendan Tierney <tiernebre@gmail.com>
2021-04-25 23:04:42 -0600

    WAY more responsive button

--- packages/front-end/src/components/layout/Header.tsx
+++ packages/front-end/src/components/layout/Header.tsx
@@ -8,23 +8,21 @@ interface HeaderProps {
 }
 
 export const Header = ({ onLogOut, isAuthenticated }: HeaderProps) => (
-  <header className="Header columns is-vcentered is-mobile is-centered">
-    <div className="column is-6">
+  <header className="Header columns">
+    <div className="column is-11-desktop">
       <Link to="/">
         <img src={logo} className="mt-4 ml-4" alt="Logo" />
       </Link>
     </div>
-    <div className="column is-6">
-      <div className="is-pulled-right">
-        {isAuthenticated && (
-          <button
-            className="Header__log-out-button button is-danger"
-            onClick={onLogOut}
-          >
-            Log Out
-          </button>
-        )}
-      </div>
+    <div className="column is-1-desktop Header__log-out-button-column">
+      {isAuthenticated && (
+        <button
+          className="Header__log-out-button button is-danger"
+          onClick={onLogOut}
+        >
+          Log Out
+        </button>
+      )}
     </div>
   </header>
 );

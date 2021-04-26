ab6b35e101bc96243e8794b9ae8705ab9caa0ded
Brendan Tierney <tiernebre@gmail.com>
2021-04-25 20:00:19 -0600

    reformatted code

--- packages/back-end/src/challenge/challenge.service.int.spec.ts
+++ packages/back-end/src/challenge/challenge.service.int.spec.ts
@@ -1,14 +1,14 @@
 import { createConnection, getRepository, Repository } from "typeorm";
 import { ChallengeEntity } from "./challenge.entity";
 import { ChallengeService } from "./challenge.service";
-import { seedChallenges } from './challenge.seed';
+import { seedChallenges } from "./challenge.seed";
 import { UserEntity } from "../user/user.entity";
-import { seedUsers } from '../user/user.seed';
+import { seedUsers } from "../user/user.seed";
 
 describe("ChallengeService (integration)", () => {
   let challengeService: ChallengeService;
-  let challengeRepository: Repository<ChallengeEntity>
-  let userRepository: Repository<UserEntity>
+  let challengeRepository: Repository<ChallengeEntity>;
+  let userRepository: Repository<UserEntity>;
 
   beforeAll(async () => {
     const connection = await createConnection({
@@ -18,34 +18,37 @@ describe("ChallengeService (integration)", () => {
       port: 5433,
       entities: ["src/**/*.entity.ts"],
       synchronize: true,
-      logging: false
+      logging: false,
     });
     await connection.query("SELECT 1");
-    challengeRepository = getRepository(ChallengeEntity)
-    userRepository = getRepository(UserEntity)
-    await seedChallenges(challengeRepository)
-    await seedUsers(userRepository)
+    challengeRepository = getRepository(ChallengeEntity);
+    userRepository = getRepository(UserEntity);
+    await seedChallenges(challengeRepository);
+    await seedUsers(userRepository);
   });
 
   beforeEach(() => {
     challengeService = new ChallengeService(challengeRepository);
   });
 
-  describe('getAllForCurrentUser', () => {
+  describe("getAllForCurrentUser", () => {
     it("returns all of the challenges that are only tied to a user", async () => {
-      const challenges = await challengeRepository.findByIds([1, 3])
-      const user = await userRepository.findOne() as UserEntity
-      challenges.forEach(challenge => {
-        challenge.users = Promise.resolve([user])
-      })
-      await challengeRepository.save(challenges)
-      const challengesFound = await challengeService.getAllForCurrentUser({ id: user.id, accessKey: user.accessKey })
-      expect(challengesFound).toHaveLength(2)
-      const [firstChallenge, secondChallenge] = challengesFound
-      expect(firstChallenge.id).toEqual(challenges[0].id)
-      expect(firstChallenge.name).toEqual(challenges[0].name)
-      expect(secondChallenge.id).toEqual(challenges[1].id)
-      expect(secondChallenge.name).toEqual(challenges[1].name)
-    })
-  })
+      const challenges = await challengeRepository.findByIds([1, 3]);
+      const user = (await userRepository.findOne()) as UserEntity;
+      challenges.forEach((challenge) => {
+        challenge.users = Promise.resolve([user]);
+      });
+      await challengeRepository.save(challenges);
+      const challengesFound = await challengeService.getAllForCurrentUser({
+        id: user.id,
+        accessKey: user.accessKey,
+      });
+      expect(challengesFound).toHaveLength(2);
+      const [firstChallenge, secondChallenge] = challengesFound;
+      expect(firstChallenge.id).toEqual(challenges[0].id);
+      expect(firstChallenge.name).toEqual(challenges[0].name);
+      expect(secondChallenge.id).toEqual(challenges[1].id);
+      expect(secondChallenge.name).toEqual(challenges[1].name);
+    });
+  });
 });
--- packages/back-end/src/user/user.seed.ts
+++ packages/back-end/src/user/user.seed.ts
@@ -3,14 +3,17 @@ import { generateRandomString } from "../random";
 import { UserEntity } from "./user.entity";
 import { v4 as uuidv4 } from "uuid";
 
-export const seedUsers = async (repository: Repository<UserEntity>, count = 20): Promise<UserEntity[]> => {
-  const users = []
+export const seedUsers = async (
+  repository: Repository<UserEntity>,
+  count = 20
+): Promise<UserEntity[]> => {
+  const users = [];
   for (let i = 0; i < count; i++) {
-    const user = repository.create()
-    user.nickname = generateRandomString()
-    user.password = generateRandomString()
-    user.accessKey = uuidv4()
-    users.push(user)
+    const user = repository.create();
+    user.nickname = generateRandomString();
+    user.password = generateRandomString();
+    user.accessKey = uuidv4();
+    users.push(user);
   }
-  return repository.save(users)
-}
+  return repository.save(users);
+};

dc6764afc2b57e69b3df034d8fbb51ec44e16f6c
Brendan Tierney <tiernebre@gmail.com>
2021-04-25 22:48:45 -0600

    better spacing

--- packages/front-end/src/components/layout/Header.tsx
+++ packages/front-end/src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 export const Header = ({ onLogOut, isAuthenticated }: HeaderProps) => (
   <header className="Header columns is-vcentered is-mobile is-centered">
     <div className="column is-6">
-      <img src={logo} className="mt-3" alt="Logo"/>
+      <img src={logo} className="mt-4 ml-4" alt="Logo"/>
     </div>
     <div className="column is-6">
       <div className="is-pulled-right">

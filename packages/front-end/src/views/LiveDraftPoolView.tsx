4f350d8ca99f3b257766e5db09f414b1e0fe05e4
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 22:46:38 -0600

    loading live draft setup

--- packages/front-end/src/views/LiveDraftPoolView.tsx
+++ packages/front-end/src/views/LiveDraftPoolView.tsx
@@ -7,14 +7,15 @@ import { PooledPokemon } from "../components/pokemon/PooledPokemon";
 import "./LiveDraftPoolView.scss";
 
 type DraftParams = {
-  draftId?: string
-}
+  draftId?: string;
+};
 
 export const LiveDraftPoolView = () => {
-  const { draftId } = useParams<DraftParams>()
-
+  const { draftId } = useParams<DraftParams>();
   const { lastMessage, readyState } = useWebSocket(
-    `${process.env.REACT_APP_BACK_END_API_WS_URL}/drafts/${Number(draftId)}/live-pool`
+    `${process.env.REACT_APP_BACK_END_API_WS_URL}/drafts/${Number(
+      draftId
+    )}/live-pool`
   );
 
   const isReady = () => readyState === ReadyState.OPEN;
@@ -34,17 +35,21 @@ export const LiveDraftPoolView = () => {
   return (
     <Fragment>
       <h1 className="title">Live Draft Pool</h1>
-      <div className="LiveDraftPoolView columns">
-        <div className="LiveDraftPoolView__pooled-pokemon-column column is-2 is-12-mobile">
-          <PooledPokemon pokemon={pooledPokemon} />
+      {currentDraftStatus ? (
+        <div className="LiveDraftPoolView columns">
+          <div className="LiveDraftPoolView__pooled-pokemon-column column is-2 is-12-mobile">
+            <PooledPokemon pokemon={pooledPokemon} />
+          </div>
+          <div className="column is-10">
+            <PokemonInformation
+              pokemon={currentPokemon}
+              emptyPlaceholder="The Pool is being loaded..."
+            />
+          </div>
         </div>
-        <div className="column is-10">
-          <PokemonInformation
-            pokemon={currentPokemon}
-            emptyPlaceholder="The Pool is being loaded..."
-          />
-        </div>
-      </div>
+      ) : (
+        <p>Loading Live Draft...</p>
+      )}
     </Fragment>
   );
 };

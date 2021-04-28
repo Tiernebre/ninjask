3637a28ec6010db2d5f75b0c5be5ac915d217c9b
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 21:09:16 -0600

    test image rendering

--- packages/front-end/src/components/pokemon/PooledPokemon.spec.tsx
+++ packages/front-end/src/components/pokemon/PooledPokemon.spec.tsx
@@ -2,24 +2,26 @@ import { render, screen } from '@testing-library/react'
 import { Pokemon } from '../../api/pokemon'
 import { PooledPokemon } from './PooledPokemon'
 
-
-it("displays the pokemon names given to it", () => {
+it("correctly renders the pokemon given to it", () => {
   const pokemon: Pokemon[] = [
     {
       id: 1,
       name: 'Pikachu',
-      imageUrl: '',
-      iconUrl: ''
+      imageUrl: 'pikachu.jpg',
+      iconUrl: 'pikachu-icon.png'
     },
     {
       id: 2,
       name: 'Squirtle',
-      imageUrl: '',
-      iconUrl: ''
+      imageUrl: 'squirtle.jpg',
+      iconUrl: 'squirtle-icon.png'
     }
   ]
   render(<PooledPokemon pokemon={pokemon} />)
   pokemon.forEach(individualPokemon => {
     expect(screen.getByText(individualPokemon.name)).toBeInTheDocument()
+    const pokemonImage = screen.getByAltText(individualPokemon.name)
+    expect(pokemonImage).toBeInTheDocument()
+    expect(pokemonImage).toHaveAttribute('src', individualPokemon.iconUrl)
   })
 })

c0cbfe2217e1b812e0a19127b8a06bc22ec97e62
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 22:35:06 -0600

    better user experience let's go

--- packages/front-end/src/views/LiveDraftPoolView.tsx
+++ packages/front-end/src/views/LiveDraftPoolView.tsx
@@ -27,11 +27,11 @@ export const LiveDraftPoolView = () => {
   return (
     <Fragment>
       <h1 className="title">Live Draft Pool</h1>
-      <div className="LiveDraftPoolView columns is-mobile">
-        <div className="column is-12-mobile">
+      <div className="LiveDraftPoolView columns">
+        <div className="LiveDraftPoolView__pooled-pokemon-column column is-2 is-12-mobile">
           <PooledPokemon pokemon={pooledPokemon} />
         </div>
-        <div className="column">
+        <div className="column is-10">
           <PokemonInformation
             pokemon={currentPokemon}
             emptyPlaceholder="The Pool is being loaded..."

ab20d127d48be08546bc8dbb9fb76a0af73c3ea9
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 23:17:15 -0600

    generate migration file

---
+++ packages/back-end/src/db/migrations/1619586998217-MigrateFromSynchronize.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class MigrateFromSynchronize1619586998217 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

2bf4e1e5270739fc132fa45c0e2ce04e4c3ea46f
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 20:51:07 -0600

    add constraint check for DB

--- packages/back-end/src/draft/draft.entity.ts
+++ packages/back-end/src/draft/draft.entity.ts
@@ -1,4 +1,5 @@
 import {
+  Check,
   Column,
   Entity,
   JoinColumn,
@@ -12,6 +13,8 @@ import { DraftPokemonEntity } from "./draft-pokemon.entity";
 @Entity({
   name: "draft",
 })
+@Check(`"live_pool_pokemon_index" >= -1`)
+@Check(`"live_pool_pokemon_index" < "pool_size"`)
 export class DraftEntity {
   @PrimaryGeneratedColumn()
   id!: number;

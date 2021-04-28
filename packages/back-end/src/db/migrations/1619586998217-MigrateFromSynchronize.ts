0dce3edfeca2c1896a1cf6be6befc90310e31017
Brendan Tierney <tiernebre@gmail.com>
2021-04-27 23:17:25 -0600

    auto format

--- packages/back-end/src/db/migrations/1619586998217-MigrateFromSynchronize.ts
+++ packages/back-end/src/db/migrations/1619586998217-MigrateFromSynchronize.ts
@@ -1,11 +1,7 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MigrateFromSynchronize1619586998217 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }

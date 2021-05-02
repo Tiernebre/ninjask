bc926fcb3f886ea013d7dd4b25bee89cf5b4d952
Brendan Tierney <tiernebre@gmail.com>
2021-05-02 10:29:12 -0600

    TDD Test for access key generation

---
+++ packages/back-end/src/user/user.service.int.spec.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from "typeorm";
+import { UserEntity } from "./user.entity";
+import { establishDbConnection } from "../test/create-db-connection";
+import { UserService } from './user.service';
+import { object, when } from 'testdouble'
+import { PasswordEncoder } from "../crypto/password-encoder";
+import { CreateUserRequest } from "./create-user-request";
+import { generateRandomNumber, generateRandomString } from "../random";
+
+describe("UserService (integration)", () => {
+  let userService: UserService;
+  let userRepository: Repository<UserEntity>;
+  let passwordEncoder: PasswordEncoder
+
+  beforeAll(async () => {
+    await establishDbConnection();
+    userRepository = getRepository(UserEntity)
+  });
+
+  beforeEach(() => {
+    passwordEncoder = object<PasswordEncoder>()
+    userService = new UserService(
+      passwordEncoder,
+      userRepository
+    )
+  });
+
+  describe("createOne", () => {
+    it('creates an access key for the user', async () => {
+      const createUserRequest: CreateUserRequest = {
+        nickname: `Test User ${generateRandomNumber()}`,
+        password: generateRandomString()
+      }
+      when(passwordEncoder.encode(createUserRequest.password)).thenResolve(createUserRequest.password)
+      const createdUser = await userService.createOne(createUserRequest)
+      expect(createdUser.accessKey).toBeTruthy()
+    })
+  })
+})

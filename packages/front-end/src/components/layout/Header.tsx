66e9f0a0048f50338b476821fdb180a4f83fea8a
Brendan Tierney <tiernebre@gmail.com>
2021-05-02 12:31:34 -0600

    responsive header styling

--- packages/front-end/src/components/layout/Header.tsx
+++ packages/front-end/src/components/layout/Header.tsx
@@ -9,12 +9,12 @@ interface HeaderProps {
 
 export const Header = ({ onLogOut, isAuthenticated }: HeaderProps) => (
   <header className="Header columns p-3">
-    <div className="column is-11">
+    <div className="column Header__logo-column">
       <Link to="/">
         <img src={logo} alt="Logo" />
       </Link>
     </div>
-    <div className="column is-1">
+    <div className="column Header__log-out-button-column">
       {isAuthenticated && (
         <button
           className="Header__log-out-button button is-danger"

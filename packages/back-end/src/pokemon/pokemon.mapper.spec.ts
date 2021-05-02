e164c988cf40a30290916f729d41cddb83a18a90
Brendan Tierney <tiernebre@gmail.com>
2021-05-02 16:05:51 -0600

    add new mapping property for image urls

--- packages/back-end/src/pokemon/pokemon.mapper.spec.ts
+++ packages/back-end/src/pokemon/pokemon.mapper.spec.ts
@@ -10,10 +10,10 @@ describe("pokemon.mapper", () => {
       const pokemonCreated = mapFromPokeApi(pokeApiPokemon);
       expect(pokemonCreated.id).toEqual(pokeApiPokemon.id);
       expect(pokemonCreated.name).toEqual(pokeApiPokemon.name);
-      expect(pokemonCreated.imageUrl).toEqual(
+      expect(pokemonCreated.imageUrls.highResImage).toEqual(
         "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
       );
-      expect(pokemonCreated.iconUrl).toEqual(
+      expect(pokemonCreated.imageUrls.icon).toEqual(
         "https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular/pikachu.png"
       );
     });
@@ -22,7 +22,7 @@ describe("pokemon.mapper", () => {
       const pokeApiPokemon = generateMockPokeApiPokemonSpecies();
       pokeApiPokemon.id = 15;
       const pokemonCreated = mapFromPokeApi(pokeApiPokemon);
-      expect(pokemonCreated.imageUrl).toEqual(
+      expect(pokemonCreated.imageUrls.highResImage).toEqual(
         "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/015.png"
       );
     });
@@ -31,7 +31,7 @@ describe("pokemon.mapper", () => {
       const pokeApiPokemon = generateMockPokeApiPokemonSpecies();
       pokeApiPokemon.id = 354;
       const pokemonCreated = mapFromPokeApi(pokeApiPokemon);
-      expect(pokemonCreated.imageUrl).toEqual(
+      expect(pokemonCreated.imageUrls.highResImage).toEqual(
         "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/354.png"
       );
     });
--- packages/back-end/src/pokemon/pokemon.mapper.ts
+++ packages/back-end/src/pokemon/pokemon.mapper.ts
@@ -1,28 +1,43 @@
 import { PokeApiPokemonSpecies } from "../poke-api";
 import { Pokemon } from "./pokemon";
+import { PokemonImageUrls } from "./pokemon-image-urls";
 
 const POKEMON_IMAGE_URL =
   "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images";
 const POKEMON_ICON_IMAGE_URL =
   "https://raw.githubusercontent.com/msikma/pokesprite/master/icons/pokemon/regular";
+const POKEMON_THUMBNAIL_IMAGE_URL =
+  "https://github.com/HybridShivam/Pokemon/tree/master/assets/thumbnails-compressed";
+
+const getPaddedPokemonId = (pokemon: PokeApiPokemonSpecies): string => {
+  const id = pokemon.id.toString();
+  return id.padStart(3, "0");
+}
 
 const getImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
-  const id = pokemon.id.toString();
-  const paddedId = id.padStart(3, "0");
-  return `${POKEMON_IMAGE_URL}/${paddedId}.png`;
+  return `${POKEMON_IMAGE_URL}/${getPaddedPokemonId(pokemon)}.png`;
 };
 
 const getIconImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
   return `${POKEMON_ICON_IMAGE_URL}/${pokemon.name.toLowerCase()}.png`;
 };
 
+const getThumbnailImageUrlForPokemon = (pokemon: PokeApiPokemonSpecies): string => {
+  return `${POKEMON_THUMBNAIL_IMAGE_URL}/${getPaddedPokemonId(pokemon)}.png`;
+};
+
+const getImageUrlsForPokeApiPokemon = (pokeApiPokemon: PokeApiPokemonSpecies): PokemonImageUrls => {
+  return {
+    icon: getIconImageUrlForPokemon(pokeApiPokemon),
+    highResImage: getImageUrlForPokemon(pokeApiPokemon),
+    thumbnailImage: getThumbnailImageUrlForPokemon(pokeApiPokemon)
+  }
+}
+
 export const mapFromPokeApi = (
   pokeApiPokemon: PokeApiPokemonSpecies
-): Pokemon => {
-  return new Pokemon(
-    pokeApiPokemon.id,
-    pokeApiPokemon.name,
-    getImageUrlForPokemon(pokeApiPokemon),
-    getIconImageUrlForPokemon(pokeApiPokemon)
-  );
-};
+): Pokemon => ({
+  id: pokeApiPokemon.id,
+  name: pokeApiPokemon.name,
+  imageUrls: getImageUrlsForPokeApiPokemon(pokeApiPokemon)
+});

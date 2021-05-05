42b106866401f33c45a38cc8450126907256dd4e
Brendan Tierney <tiernebre@gmail.com>
2021-05-04 18:41:10 -0600

    reformatting

--- packages/back-end/src/db/migrations/1620174086272-BetterLiveDraftIndexConstraints.ts
+++ packages/back-end/src/db/migrations/1620174086272-BetterLiveDraftIndexConstraints.ts
@@ -8,7 +8,7 @@ export class BetterLiveDraftIndexConstraints1620174086272
         ALTER TABLE IF EXISTS draft ADD CONSTRAINT live_pool_pokemon_index_greater_than_constraint CHECK (live_pool_pokemon_index >= -1);
         ALTER TABLE IF EXISTS draft ADD CONSTRAINT live_pool_pokemon_index_lesser_than_constraint CHECK (live_pool_pokemon_index < pool_size);
       `
-    )
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -17,6 +17,6 @@ export class BetterLiveDraftIndexConstraints1620174086272
         ALTER TABLE IF EXISTS draft DROP CONSTRAINT live_pool_pokemon_index_greater_than_constraint;
         ALTER TABLE IF EXISTS draft DROP CONSTRAINT live_pool_pokemon_index_lesser_than_constraint;
       `
-    )
+    );
   }
 }

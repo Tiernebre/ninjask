dd20d59c64659ffaede72adfd114b799c99612c7
Brendan Tierney <tiernebre@gmail.com>
2021-05-04 18:41:04 -0600

    add constraint check

--- packages/back-end/src/db/migrations/1620174086272-BetterLiveDraftIndexConstraints.ts
+++ packages/back-end/src/db/migrations/1620174086272-BetterLiveDraftIndexConstraints.ts
@@ -2,7 +2,21 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class BetterLiveDraftIndexConstraints1620174086272
   implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `
+        ALTER TABLE IF EXISTS draft ADD CONSTRAINT live_pool_pokemon_index_greater_than_constraint CHECK (live_pool_pokemon_index >= -1);
+        ALTER TABLE IF EXISTS draft ADD CONSTRAINT live_pool_pokemon_index_lesser_than_constraint CHECK (live_pool_pokemon_index < pool_size);
+      `
+    )
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `
+        ALTER TABLE IF EXISTS draft DROP CONSTRAINT live_pool_pokemon_index_greater_than_constraint;
+        ALTER TABLE IF EXISTS draft DROP CONSTRAINT live_pool_pokemon_index_lesser_than_constraint;
+      `
+    )
+  }
 }

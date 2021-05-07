aaf0d9cc31e9a28b19fe2eff29f3c2f4a35ab1d7
Brendan Tierney <tiernebre@gmail.com>
2021-05-06 18:47:24 -0600

    migration script for creator id

--- packages/back-end/src/db/migrations/1620348277649-AssociateChallengeWithOwner.ts
+++ packages/back-end/src/db/migrations/1620348277649-AssociateChallengeWithOwner.ts
@@ -2,7 +2,15 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AssociateChallengeWithOwner1620348277649
   implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(
+          `ALTER TABLE IF EXISTS challenge ADD COLUMN IF NOT EXISTS creator_id NOT NULL REFERENCES users (id);`
+      )
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(
+          `ALTER TABLE IF EXISTS challenge DROP COLUMN IF EXISTS creator_id;`
+      )
+  }
 }

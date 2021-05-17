f2dcabcdd41992f8c1ae54c9943b2c3c24f3ae63
Brendan Tierney <tiernebre@gmail.com>
2021-05-16 19:09:04 -0600

    WS test

--- packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
+++ packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
@@ -7,23 +7,24 @@ import { ContextState } from '../types/state';
 import { AddressInfo } from 'net';
 import WebSocket from 'ws';
 import { Server } from 'http';
+import { object, when } from 'testdouble'
+import { generateMockLiveDraftStatus } from './draft.mock'
 
 describe('LiveDraftPoolMiddleware', () => {
   let liveDraftPoolService: LiveDraftPoolService;
   let app: KoaWebsocket.App<ContextState>;
   let server: Server;
   let ws: WebSocket
+  const id = 1
+  let url: string
 
-  beforeAll((done) => {
+  beforeAll(() => {
+    liveDraftPoolService = object<LiveDraftPoolService>()
     app = websockify(new Koa());
     app.ws.use(liveDraftPoolMiddleware(liveDraftPoolService, app));
     server = app.listen()
     const address = server.address() as AddressInfo
-    const url = `ws://localhost:${address.port}`
-    ws = new WebSocket(url)
-    ws.on('open', () => {
-      done()
-    })
+    url = `ws://localhost:${address.port}/drafts/${id}/live-pool`
   })
 
   afterAll(() => {
@@ -31,7 +32,16 @@ describe('LiveDraftPoolMiddleware', () => {
     ws.close()
   })
 
-  it('immediately sends a response of the current live draft pool status when connection is established', async () => {
-    expect(2).toEqual(2)
+  it('immediately sends a response of the current live draft pool status when connection is established', (done) => {
+    const liveDraftPool = generateMockLiveDraftStatus()
+    when(liveDraftPoolService.getLiveDraftPoolForOneWithId(id)).thenResolve(liveDraftPool)
+    ws = new WebSocket(url)
+    ws.on('open', () => {
+      ws.send('hello')
+    })
+    ws.on('message', data => {
+      expect(data).toEqual(JSON.stringify(liveDraftPool))
+      done()
+    })
   })
 })

7bc395e8fa0a31b8fb8f5db97e7950ce382fdcdf
Brendan Tierney <tiernebre@gmail.com>
2021-05-16 18:58:49 -0600

    ws based test

--- packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
+++ packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
@@ -1,32 +1,37 @@
-import { liveDraftPoolMiddleware } from './live-draft-pool.middleware'
 import { LiveDraftPoolService } from './live-draft-pool.service'
 import Koa from "koa";
-import KoaWebsocket from "koa-websocket";
-import { ContextState } from "../types/state";
-import { object, when, verify } from 'testdouble'
-import { generateMockLiveDraftStatus } from './draft.mock';
-import flushPromises from 'flush-promises';
+import websockify from "koa-websocket";
+import KoaWebsocket from 'koa-websocket';
+import { liveDraftPoolMiddleware } from './live-draft-pool.middleware'
+import { ContextState } from '../types/state';
+import { AddressInfo } from 'net';
+import WebSocket from 'ws';
+import { Server } from 'http';
 
 describe('LiveDraftPoolMiddleware', () => {
   let liveDraftPoolService: LiveDraftPoolService;
   let app: KoaWebsocket.App<ContextState>;
-  let middleware: Koa.Middleware;
+  let server: Server;
+  let ws: WebSocket
 
-  beforeEach(() => {
-    liveDraftPoolService = object<LiveDraftPoolService>()
-    app = object<KoaWebsocket.App<ContextState>>()
-    middleware = liveDraftPoolMiddleware(liveDraftPoolService, app)
+  beforeAll((done) => {
+    app = websockify(new Koa());
+    app.ws.use(liveDraftPoolMiddleware(liveDraftPoolService, app));
+    server = app.listen()
+    const address = server.address() as AddressInfo
+    const url = `ws://localhost:${address.port}`
+    ws = new WebSocket(url)
+    ws.on('open', () => {
+      done()
+    })
+  })
+
+  afterAll(() => {
+    server.close()
+    ws.close()
   })
 
   it('immediately sends a response of the current live draft pool status when connection is established', async () => {
-    const id = 1
-    const liveDraftStatus = generateMockLiveDraftStatus()
-    when(liveDraftPoolService.getLiveDraftPoolForOneWithId(id)).thenResolve(liveDraftStatus)
-    const ctx = object<Koa.Context>()
-    ctx.path = `/drafts/${id}/live-pool`
-    const next = jest.fn()
-    middleware(ctx, next)
-    await flushPromises()
-    verify(ctx.websocket.send(JSON.stringify(liveDraftStatus)), { times: 1 })
+    expect(2).toEqual(2)
   })
 })

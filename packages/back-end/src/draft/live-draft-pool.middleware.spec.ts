abd65a83835ebc47018d40c5d688685406e1f9d4
Brendan Tierney <tiernebre@gmail.com>
2021-05-16 19:19:48 -0600

    testing the middleware

--- packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
+++ packages/back-end/src/draft/live-draft-pool.middleware.spec.ts
@@ -7,7 +7,7 @@ import { ContextState } from '../types/state';
 import { AddressInfo } from 'net';
 import WebSocket from 'ws';
 import { Server } from 'http';
-import { object, when } from 'testdouble'
+import { object, reset, verify, when } from 'testdouble'
 import { generateMockLiveDraftStatus } from './draft.mock'
 
 describe('LiveDraftPoolMiddleware', () => {
@@ -29,7 +29,11 @@ describe('LiveDraftPoolMiddleware', () => {
 
   afterAll(() => {
     server.close()
+  })
+
+  afterEach(() => {
     ws.close()
+    reset()
   })
 
   it('immediately sends a response of the current live draft pool status when connection is established', (done) => {
@@ -41,6 +45,40 @@ describe('LiveDraftPoolMiddleware', () => {
     })
     ws.on('message', data => {
       expect(data).toEqual(JSON.stringify(liveDraftPool))
+      done()
+    })
+  })
+
+  it('advances to the next pokemon in the live draft pool when the NEXT message is received', (done) => {
+    const liveDraftPool = generateMockLiveDraftStatus()
+    const newLiveDraftPool = generateMockLiveDraftStatus()
+    when(liveDraftPoolService.getLiveDraftPoolForOneWithId(id)).thenResolve(liveDraftPool)
+    when(liveDraftPoolService.revealNextPokemonInLivePoolForId(id)).thenResolve(newLiveDraftPool)
+    ws = new WebSocket(url)
+    let numberOfMessages = 0
+    ws.on('open', () => {
+      ws.send('NEXT')
+    })
+    ws.on('message', data => {
+      numberOfMessages++
+      if (numberOfMessages === 2) {
+        expect(data).toEqual(JSON.stringify(newLiveDraftPool))
+        done()
+      }
+    })
+  })
+
+  it('does nothing if an unsupported message is received', (done) => {
+    const liveDraftPool = generateMockLiveDraftStatus()
+    when(liveDraftPoolService.getLiveDraftPoolForOneWithId(id)).thenResolve(liveDraftPool)
+    ws = new WebSocket(url)
+    ws.on('open', () => {
+      for (let i = 0; i++; i < 3) {
+        ws.send(`Some Random Message ${i}. Hello there!`)
+      }
+    })
+    ws.on('message', () => {
+      verify(liveDraftPoolService.revealNextPokemonInLivePoolForId(id), { times: 0 })
       done()
     })
   })

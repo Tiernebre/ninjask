74fd5c027f003035704bd4adc67eea7101423850
Brendan Tierney <tiernebre@gmail.com>
2021-05-18 21:19:58 -0600

    test user length

--- packages/back-end/src/challenge/challenge-participant.service.int.spec.ts
+++ packages/back-end/src/challenge/challenge-participant.service.int.spec.ts
@@ -6,7 +6,7 @@ import { ChallengeParticipantEntity } from "./challenge-participant.entity";
 import { ChallengeParticipantService } from './challenge-participant.service';
 import { ChallengeEntity } from "./challenge.entity";
 import { seedChallengeParticipants, seedChallenges } from "./challenge.seed";
-import { orderBy } from 'lodash'
+import { orderBy, sample } from 'lodash'
 import { ChallengeResult } from "./challenge-result";
 
 describe('ChallengeParticipantService (integration)', () => {
@@ -31,6 +31,10 @@ describe('ChallengeParticipantService (integration)', () => {
     challengeParticipantService = new ChallengeParticipantService(challengeParticipantRepository);
   });
 
+  afterEach(async () => {
+    await challengeParticipantRepository.createQueryBuilder().delete().execute()
+  })
+
   describe("getCompletedResultsForChallengeInOrder", () => {
     it("returns the participants in order from least to longest time", async () => {
       expect(users.length).toBeGreaterThan(2)
@@ -49,6 +53,36 @@ describe('ChallengeParticipantService (integration)', () => {
       const gottenResults = await challengeParticipantService.getCompletedResultsForChallengeInOrder(challenge.id)
       expect(gottenResults).toHaveLength(users.length)
       expect(gottenResults).toEqual(expectedChallengeResults)
+    })
+
+    it("does not return participants who did not enter in a time", async () => {
+      expect(users.length).toBeGreaterThan(2)
+      const userWithoutEnteredTime = sample(users) as UserEntity
+      const [challenge] = challenges
+      let expectedChallengeResults: ChallengeResult[] = []
+      for (const user of users) {
+        const [challengeParticipant] = await seedChallengeParticipants(challengeParticipantRepository, [challenge], user)
+        if (user !== userWithoutEnteredTime) {
+          expectedChallengeResults.push({
+            participantId: challengeParticipant.id,
+            completionTimeHour: challengeParticipant.completionTimeHour,
+            completionTimeMinutes: challengeParticipant.completionTimeMinutes,
+            nickname: user.nickname
+          })
+        } else {
+          challengeParticipant.completionTimeHour = null
+          challengeParticipant.completionTimeMinutes = null
+          await challengeParticipantRepository.save(challengeParticipant)
+        }
+      }
+      expectedChallengeResults = orderBy(expectedChallengeResults, ['completionTimeHour', 'completionTimeMinutes'], ['asc', 'asc'])
+      const gottenResults = await challengeParticipantService.getCompletedResultsForChallengeInOrder(challenge.id)
+      expect(gottenResults).toHaveLength(users.length - 1)
+      expect(gottenResults).toEqual(expectedChallengeResults)
+      const foundNonEnteredResult = expectedChallengeResults.find(result => 
+        result.nickname === userWithoutEnteredTime.nickname
+      )
+      expect(foundNonEnteredResult).toBeUndefined()
     })
   })
 })

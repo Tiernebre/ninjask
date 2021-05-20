5c84d53c9edfe58aa6f40bd37c8a219ab05cd1b4
Brendan Tierney <tiernebre@gmail.com>
2021-05-20 17:49:07 -0600

    unit tests for challenge result

--- packages/front-end/src/components/challenge/challenge-results/ChallengeResult.spec.tsx
+++ packages/front-end/src/components/challenge/challenge-results/ChallengeResult.spec.tsx
@@ -15,4 +15,19 @@ describe("ChallengeResult", () => {
     render(<ChallengeResult result={challengeResult} rank={rank} />)
     expect(screen.getByText(`${rank}. ${challengeResult.nickname}`)).toBeInTheDocument()
   })
+
+  it("displays the completion time correctly (single digit hour and minutes)", () => {
+    const challengeResult: ChallengeResultTyping = {
+      participantId: 1,
+      resultId: 1,
+      completionTimeHour: 1,
+      completionTimeMinutes: 1,
+      nickname: 'Test'
+    }
+    const rank = 2
+    render(<ChallengeResult result={challengeResult} rank={rank} />)
+    const paddedHour = `0${challengeResult.completionTimeHour}`
+    const paddedMinutes = `0${challengeResult.completionTimeMinutes}`
+    expect(screen.getByText(`${paddedHour}:${paddedMinutes}`)).toBeInTheDocument()
+  })
 })

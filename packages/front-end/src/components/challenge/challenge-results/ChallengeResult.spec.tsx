8dc885eaa5a1309d7c6caf807f5b0c8274b40749
Brendan Tierney <tiernebre@gmail.com>
2021-05-20 17:49:37 -0600

    cleanup formatting

--- packages/front-end/src/components/challenge/challenge-results/ChallengeResult.spec.tsx
+++ packages/front-end/src/components/challenge/challenge-results/ChallengeResult.spec.tsx
@@ -2,32 +2,30 @@ import { render, screen } from "@testing-library/react"
 import { ChallengeResult as ChallengeResultTyping } from "../../../api/challenge/ChallengeResult"
 import { ChallengeResult } from "./ChallengeResult"
 
-describe("ChallengeResult", () => {
-  it("displays the rank correctly", () => {
-    const challengeResult: ChallengeResultTyping = {
-      participantId: 1,
-      resultId: 1,
-      completionTimeHour: 1,
-      completionTimeMinutes: 1,
-      nickname: 'Test'
-    }
-    const rank = 2
-    render(<ChallengeResult result={challengeResult} rank={rank} />)
-    expect(screen.getByText(`${rank}. ${challengeResult.nickname}`)).toBeInTheDocument()
-  })
+it("displays the rank correctly", () => {
+  const challengeResult: ChallengeResultTyping = {
+    participantId: 1,
+    resultId: 1,
+    completionTimeHour: 1,
+    completionTimeMinutes: 1,
+    nickname: 'Test'
+  }
+  const rank = 2
+  render(<ChallengeResult result={challengeResult} rank={rank} />)
+  expect(screen.getByText(`${rank}. ${challengeResult.nickname}`)).toBeInTheDocument()
+})
 
-  it("displays the completion time correctly (single digit hour and minutes)", () => {
-    const challengeResult: ChallengeResultTyping = {
-      participantId: 1,
-      resultId: 1,
-      completionTimeHour: 1,
-      completionTimeMinutes: 1,
-      nickname: 'Test'
-    }
-    const rank = 2
-    render(<ChallengeResult result={challengeResult} rank={rank} />)
-    const paddedHour = `0${challengeResult.completionTimeHour}`
-    const paddedMinutes = `0${challengeResult.completionTimeMinutes}`
-    expect(screen.getByText(`${paddedHour}:${paddedMinutes}`)).toBeInTheDocument()
-  })
+it("displays the completion time correctly (single digit hour and minutes)", () => {
+  const challengeResult: ChallengeResultTyping = {
+    participantId: 1,
+    resultId: 1,
+    completionTimeHour: 1,
+    completionTimeMinutes: 1,
+    nickname: 'Test'
+  }
+  const rank = 2
+  render(<ChallengeResult result={challengeResult} rank={rank} />)
+  const paddedHour = `0${challengeResult.completionTimeHour}`
+  const paddedMinutes = `0${challengeResult.completionTimeMinutes}`
+  expect(screen.getByText(`${paddedHour}:${paddedMinutes}`)).toBeInTheDocument()
 })

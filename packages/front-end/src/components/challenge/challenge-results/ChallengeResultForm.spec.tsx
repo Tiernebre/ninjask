97b8fe48ec3f97223251c3dd2cd851130f660be4
Brendan Tierney <tiernebre@gmail.com>
2021-05-20 18:12:38 -0600

    fix test

--- packages/front-end/src/components/challenge/challenge-results/ChallengeResultForm.spec.tsx
+++ packages/front-end/src/components/challenge/challenge-results/ChallengeResultForm.spec.tsx
@@ -9,6 +9,7 @@ const getSubmitButton = () => screen.getByRole('button', { name: /Submit/i })
 const getHourErrorMessage = () => screen.getByRole('alert', { name: /Hour/i })
 const queryHourErrorMessage = () => screen.queryByRole('alert', { name: /Hour/i })
 const getMinutesErrorMessage = () => screen.getByRole('alert', { name: /Minutes/i })
+const queryMinutesErrorMessage = () => screen.queryByRole('alert', { name: /Minutes/i })
 
 it("displays an error message if the hour field is not filled out", async () => {
   const onSubmit = jest.fn()
@@ -58,4 +59,28 @@ it('displays an error message if the minutes field is not filled out', async () 
   expect(minutesErrorMessage).toHaveTextContent("Minutes are required.")
   expect(getHourInput()).toBeInvalid()
   expect(onSubmit).not.toHaveBeenCalled()
+})
+
+it.each([-1000, -1, 60, Number.MAX_SAFE_INTEGER])("displays an error message that minutes needs to be within a range when %p is provided", async (minutesToInput: number) => {
+  const onSubmit = jest.fn()
+  await act(async () => {
+    render(<ChallengeResultForm onSubmit={onSubmit} />)
+    user.type(getMinutesInput(), minutesToInput.toString())
+    user.click(getSubmitButton())
+  })
+  const minutesErrorMessage = getMinutesErrorMessage()
+  expect(minutesErrorMessage).toBeInTheDocument()
+  expect(minutesErrorMessage).toHaveTextContent("Minutes must be between 0-59.")
+  expect(getHourInput()).toBeInvalid()
+  expect(onSubmit).not.toHaveBeenCalled()
+})
+
+it.each([0, 1, 58, 59])("does not display an error message about the minutes if %p is provided", async (minutesToInput: number) => {
+  const onSubmit = jest.fn()
+  await act(async () => {
+    render(<ChallengeResultForm onSubmit={onSubmit} />)
+    user.type(getMinutesInput(), minutesToInput.toString())
+    user.click(getSubmitButton())
+  })
+  expect(queryMinutesErrorMessage()).toBeNull()
 })

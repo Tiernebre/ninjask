88eec58f5047a5f9ac5241d7362fbb362b9b90e8
Brendan Tierney <tiernebre@gmail.com>
2021-05-21 20:29:23 -0600

    more export cleanup

---
+++ packages/back-end/src/challenge-participant/index.ts
@@ -0,0 +1,4 @@
+export * from './challenge-participant'
+export * from './challenge-participant.service'
+export * from './challenge-participant.router'
+export * from './challenge-participant.entity'

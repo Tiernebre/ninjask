b490732886c9e3e4bf5ccf9d199ad1464727737f
Brendan Tierney <tiernebre@gmail.com>
2021-06-06 20:19:09 -0600

    visible menu

--- packages/front-end/src/components/navbar/Navbar.tsx
+++ packages/front-end/src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
@@ -6,47 +7,57 @@ interface NavbarProps {
   onLogOut: () => void;
 }
 
-export const Navbar = ({ isAuthenticated, onLogOut }: NavbarProps) => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link className="navbar-item" to="/">
-        <img src={logo} alt="Logo" />
-      </Link>
-    </div>
+export const Navbar = ({ isAuthenticated, onLogOut }: NavbarProps) => {
+  const [menuIsVisible, setMenuIsVisible] = useState(false)
 
-    <div id="navbarBasicExample" className="navbar-menu">
-      <div className="navbar-start">
-        <a className="navbar-item">Home</a>
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
 
-        <a className="navbar-item">Documentation</a>
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" onClick={() => setMenuIsVisible(!menuIsVisible)}>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-        <div className="navbar-item has-dropdown is-hoverable">
-          <a className="navbar-link">More</a>
+      <div className={`navbar-menu ${menuIsVisible ? 'is-active' : ''}`}>
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
 
-          <div className="navbar-dropdown">
-            <a className="navbar-item">About</a>
-            <a className="navbar-item">Jobs</a>
-            <a className="navbar-item">Contact</a>
-            <hr className="navbar-divider" />
-            <a className="navbar-item">Report an issue</a>
+          <a className="navbar-item">Documentation</a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              {isAuthenticated && (
+                <button
+                  className="Header__log-out-button button is-danger"
+                  onClick={onLogOut}
+                >
+                  Log Out
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            {isAuthenticated && (
-              <button
-                className="Header__log-out-button button is-danger"
-                onClick={onLogOut}
-              >
-                Log Out
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+}

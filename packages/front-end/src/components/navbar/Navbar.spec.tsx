6724be765823eea7ec5f5e5c34341910d547ca28
Brendan Tierney <tiernebre@gmail.com>
2021-06-06 20:41:48 -0600

    tests

--- packages/front-end/src/components/navbar/Navbar.spec.tsx
+++ packages/front-end/src/components/navbar/Navbar.spec.tsx
@@ -5,11 +5,10 @@ import { act } from 'react-dom/test-utils'
 import { MemoryRouter } from 'react-router'
 
 it('logs the user out if they are logged in', async () => {
-  const isAuthenticated = true
   const onLogOut = jest.fn()
   render(
     <MemoryRouter>
-      <Navbar isAuthenticated={isAuthenticated} onLogOut={onLogOut} />
+      <Navbar isAuthenticated={true} onLogOut={onLogOut} />
     </MemoryRouter>
   )
   const logoutButton = screen.getByRole('button', { name: /Log Out/g })
@@ -17,4 +16,14 @@ it('logs the user out if they are logged in', async () => {
     user.click(logoutButton)
   })
   expect(onLogOut).toHaveBeenCalled()
+})
+
+it('does not show the logout button if the user is not logged in', async () => {
+  render(
+    <MemoryRouter>
+      <Navbar isAuthenticated={false} onLogOut={jest.fn()} />
+    </MemoryRouter>
+  )
+  const logoutButton = screen.queryByRole('button', { name: /Log Out/g })
+  expect(logoutButton).toBeNull()
 })

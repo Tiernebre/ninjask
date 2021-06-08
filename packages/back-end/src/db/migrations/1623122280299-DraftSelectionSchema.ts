1c4128feb40b7865f2b16bd4546eb1c285476270
Brendan Tierney <tiernebre@gmail.com>
2021-06-07 21:20:44 -0600

    more specific column

--- packages/back-end/src/db/migrations/1623122280299-DraftSelectionSchema.ts
+++ packages/back-end/src/db/migrations/1623122280299-DraftSelectionSchema.ts
@@ -6,7 +6,7 @@ export class DraftSelectionSchema1623122280299 implements MigrationInterface {
         CREATE TABLE IF NOT EXISTS draft_selection (
             id SERIAL PRIMARY KEY NOT NULL,
             challenge_participant_id INTEGER REFERENCES challenge_participants (id) NOT NULL,
-            round INTEGER NOT NULL,
+            round_number INTEGER NOT NULL,
             pick_number INTEGER NOT NULL,
             pokemon_id INTEGER REFERENCES draft_pokemon (id) NULL UNIQUE,
             CONSTRAINT valid_round CHECK (round BETWEEN 1 AND 6)

d6098a789ece615bdc69a6b4628e1108e473ec84
Brendan Tierney <tiernebre@gmail.com>
2021-06-07 21:49:14 -0600

    initial service query builder

--- packages/back-end/src/draft-selection/draft-selection.service.ts
+++ packages/back-end/src/draft-selection/draft-selection.service.ts
@@ -1,9 +1,22 @@
 import { Repository } from "typeorm";
+import { DraftSelection } from "./draft-selection";
 import { DraftSelectionEntity } from "./draft-selection.entity";
+import { z } from 'zod';
 
 export class DraftSelectionService {
   constructor(
     private readonly draftSelectionRepository: Repository<DraftSelectionEntity>
   ) {}
 
+  public async getAllForDraft(draftId: number): Promise<DraftSelection[]> {
+    z.number().parse(draftId);
+    return this.draftSelectionRepository
+      .createQueryBuilder("draftSelection")
+      .innerJoin("draftSelection.challengeParticipant", "challengeParticipant")
+      .innerJoin("challengeParticipant.user", "user")
+      .innerJoin("challengeParticipant.challenge", "challenge")
+      .innerJoin("challenge.draft", "draft")
+      .where("draft.id = :draftId", { draftId })
+      .getRawMany<DraftSelection>();
+  }
 }

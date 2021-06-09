932f1877e2b70798f05844bf17ca7833b98968e9
Brendan Tierney <tiernebre@gmail.com>
2021-06-08 23:10:33 -0600

    fix migrations

--- packages/back-end/src/db/migrations/1623208726003-DraftSelectionIndexingOnRoundAndPickNumbers.ts
+++ packages/back-end/src/db/migrations/1623208726003-DraftSelectionIndexingOnRoundAndPickNumbers.ts
@@ -5,13 +5,15 @@ export class DraftSelectionIndexingOnRoundAndPickNumbers1623208726003
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE INDEX IF NOT EXISTS draft_selection_pokemon_idx ON draft_selection (pokemon_id);
+            CREATE INDEX IF NOT EXISTS draft_selection_round_idx ON draft_selection (round_number);
+            CREATE INDEX IF NOT EXISTS draft_selection_pick_idx ON draft_selection (pick_number);
         `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP INDEX IF EXISTS draft_selection_pokemon_idx;
+            DROP INDEX IF EXISTS draft_selection_round_idx;
+            DROP INDEX IF EXISTS draft_selection_pick_idx;
         `);
   }
 }
--- packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
+++ packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
@@ -5,8 +5,13 @@ export class CreateIndexForPokemonIdOnDraftSelection1623215125411
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        `);
+        CREATE INDEX IF NOT EXISTS draft_selection_pokemon_idx ON draft_selection (pokemon_id);
+    `);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DROP INDEX IF EXISTS draft_selection_pokemon_idx;
+    `);
+  }
 }

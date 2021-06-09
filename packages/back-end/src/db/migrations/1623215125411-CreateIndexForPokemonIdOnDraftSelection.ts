97c4ab704eee0059002dde2912fe33efd4100ad7
Brendan Tierney <tiernebre@gmail.com>
2021-06-08 23:07:34 -0600

    clean up code

--- packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
+++ packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
@@ -1,13 +1,12 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateIndexForPokemonIdOnDraftSelection1623215125411 implements MigrationInterface {
+export class CreateIndexForPokemonIdOnDraftSelection1623215125411
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        `);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-        `)
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }

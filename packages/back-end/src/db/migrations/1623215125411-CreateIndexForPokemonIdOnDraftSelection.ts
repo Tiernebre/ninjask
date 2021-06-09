e53744f3e1d80fdac617da52adb67712067ea4a1
Brendan Tierney <tiernebre@gmail.com>
2021-06-08 23:14:49 -0600

    include draft id index

--- packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
+++ packages/back-end/src/db/migrations/1623215125411-CreateIndexForPokemonIdOnDraftSelection.ts
@@ -6,11 +6,13 @@ export class CreateIndexForPokemonIdOnDraftSelection1623215125411
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         CREATE INDEX IF NOT EXISTS draft_selection_pokemon_idx ON draft_selection (pokemon_id);
+        CREATE INDEX IF NOT EXISTS draft_selection_draft_idx ON draft_selection (draft_id);
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
+        DROP INDEX IF EXISTS draft_selection_draft_idx;
         DROP INDEX IF EXISTS draft_selection_pokemon_idx;
     `);
   }

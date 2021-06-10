dd4e9f88b747a1c2a13334a9d4386ddef398da61
Brendan Tierney <tiernebre@gmail.com>
2021-06-09 20:51:35 -0600

    fix challenge participant router

--- packages/back-end/src/challenge-participant/challenge-participant.router.ts
+++ packages/back-end/src/challenge-participant/challenge-participant.router.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import Router from "@koa/router";
-import { ParameterizedContext } from "koa";
+import { Context } from "koa";
 import { ContextState } from "../types/state";
 import { ChallengeParticipantUpdateRequest } from "./challenge-participant-update-request";
 import { ChallengeParticipantService } from "./challenge-participant.service";
 
-export class ChallengeParticipantsRouter extends Router {
+export class ChallengeParticipantsRouter extends Router<ContextState, Context> {
   private readonly URI = "/challenge-participants";
 
   constructor(
@@ -18,7 +17,7 @@ export class ChallengeParticipantsRouter extends Router {
   private setupRoutes(): void {
     this.patch(
       `${this.URI}/:id`,
-      async (ctx: ParameterizedContext<ContextState>) => {
+      async (ctx) => {
         const request = {
           ...(<ChallengeParticipantUpdateRequest>ctx.request.body),
           id: Number(ctx.params.id),

080aac1b93947f3590d294995fba7b334ae53169
Brendan Tierney <tiernebre@gmail.com>
2021-06-09 20:22:52 -0600

    formatting

--- packages/back-end/src/db/migrations/1623291635165-PositiveConstraintOnPickNumber.ts
+++ packages/back-end/src/db/migrations/1623291635165-PositiveConstraintOnPickNumber.ts
@@ -1,17 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class PositiveConstraintOnPickNumber1623291635165 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+export class PositiveConstraintOnPickNumber1623291635165
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE draft_selection ADD CONSTRAINT positive_pick_number_check CHECK (pick_number > 0);
-        `)
-    }
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE draft_selection DROP CONSTRAINT positive_pick_number_check;
-        `)
-    }
-
+        `);
+  }
 }

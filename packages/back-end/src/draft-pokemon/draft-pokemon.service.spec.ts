4e7e6bc253d399fd3e67d1dd151f18ef5c8a1f76
Brendan Tierney <tiernebre@gmail.com>
2021-06-09 21:44:17 -0600

    code formatting

--- packages/back-end/src/draft-pokemon/draft-pokemon.service.spec.ts
+++ packages/back-end/src/draft-pokemon/draft-pokemon.service.spec.ts
@@ -1,6 +1,6 @@
 import { object, when } from "testdouble";
 import { Repository } from "typeorm";
-import { DraftPokemonService } from "."
+import { DraftPokemonService } from ".";
 import { DraftPokemonEntity } from "../draft/draft-pokemon.entity";
 import { NotFoundError } from "../error";
 import { generateMockDraftPokemon } from "./draft-pokemon.mock";
@@ -10,25 +10,27 @@ describe("DraftPokemonService", () => {
   let draftPokemonRepository: Repository<DraftPokemonEntity>;
 
   beforeEach(() => {
-    draftPokemonRepository = object<Repository<DraftPokemonEntity>>()
-    draftPokemonService = new DraftPokemonService(draftPokemonRepository)
-  })
+    draftPokemonRepository = object<Repository<DraftPokemonEntity>>();
+    draftPokemonService = new DraftPokemonService(draftPokemonRepository);
+  });
 
   describe("getOneById", () => {
     it("throws a NotFoundError if one does not exist with the provided id", async () => {
-      const id = 1
-      when(draftPokemonRepository.findOne({ id })).thenResolve(undefined)
-      await expect(draftPokemonService.getOneById(id)).rejects.toThrowError(NotFoundError)
-    })
+      const id = 1;
+      when(draftPokemonRepository.findOne({ id })).thenResolve(undefined);
+      await expect(draftPokemonService.getOneById(id)).rejects.toThrowError(
+        NotFoundError
+      );
+    });
 
     it("returns a properly mapped version of the found draft pokemon", async () => {
-      const id = 1
-      const draftPokemon = generateMockDraftPokemon()
-      when(draftPokemonRepository.findOne({ id })).thenResolve(draftPokemon)
-      const gottenPokemon = await draftPokemonService.getOneById(id)
-      expect(gottenPokemon.id).toEqual(draftPokemon.id)
-      expect(gottenPokemon.pokemonId).toEqual(draftPokemon.pokemonId)
-      expect(gottenPokemon.draftId).toEqual(draftPokemon.draftId)
-    })
-  })
-})
+      const id = 1;
+      const draftPokemon = generateMockDraftPokemon();
+      when(draftPokemonRepository.findOne({ id })).thenResolve(draftPokemon);
+      const gottenPokemon = await draftPokemonService.getOneById(id);
+      expect(gottenPokemon.id).toEqual(draftPokemon.id);
+      expect(gottenPokemon.pokemonId).toEqual(draftPokemon.pokemonId);
+      expect(gottenPokemon.draftId).toEqual(draftPokemon.draftId);
+    });
+  });
+});

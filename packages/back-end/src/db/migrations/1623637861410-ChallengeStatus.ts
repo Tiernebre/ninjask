7cc54bd59cc10de3815b7a7b151ce483f327983a
Brendan Tierney <tiernebre@gmail.com>
2021-06-13 20:34:28 -0600

    migration file for challenge status

---
+++ packages/back-end/src/db/migrations/1623637861410-ChallengeStatus.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ChallengeStatus1623637861410 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TYPE challenge_status AS ENUM ('CREATED', 'POOLED', 'DRAFTED', 'COMPLETED');
+            ALTER TABLE IF EXISTS challenge ADD COLUMN IF NOT EXISTS status challenge_status NOT NULL DEFAULT 'CREATED';
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE IF EXISTS challenge DROP COLUMN IF EXISTS status;
+            DROP TYPE challenge_status;
+        `)
+    }
+
+}

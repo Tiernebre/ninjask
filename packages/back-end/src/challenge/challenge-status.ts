4410ae56607018fe206f805be7f1084946570d26
Brendan Tierney <tiernebre@gmail.com>
2021-06-13 20:27:10 -0600

    layout challenge status structure

---
+++ packages/back-end/src/challenge/challenge-status.ts
@@ -0,0 +1,13 @@
+export enum ChallengeStatus {
+  // 1. Participants can register for a challenge that is in "CREATED" mode.
+  CREATED = "CREATED",
+
+  // 2. The associated Draft has its pool created. Registrations are closed.
+  POOLED = "POOLED",
+
+  // 3. The associated Draft has all its picks finalized. 
+  DRAFTED = "DRAFTED",
+
+  // 4. Completed, all if not most of the results for the challenge have been submitted by the participant and submissions are closed.
+  COMPLETED = "COMPLETED"
+}

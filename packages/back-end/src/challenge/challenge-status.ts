4391cdead72f348092dfd6567f5fbd0642d8edd7
Brendan Tierney <tiernebre@gmail.com>
2021-06-13 20:34:43 -0600

    migration file formatting

--- packages/back-end/src/challenge/challenge-status.ts
+++ packages/back-end/src/challenge/challenge-status.ts
@@ -5,9 +5,9 @@ export enum ChallengeStatus {
   // 2. The associated Draft has its pool created. Registrations are closed.
   POOLED = "POOLED",
 
-  // 3. The associated Draft has all its picks finalized. 
+  // 3. The associated Draft has all its picks finalized.
   DRAFTED = "DRAFTED",
 
   // 4. Completed, all if not most of the results for the challenge have been submitted by the participant and submissions are closed.
-  COMPLETED = "COMPLETED"
+  COMPLETED = "COMPLETED",
 }

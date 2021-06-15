8e720261d9122ec69ffdb8fe4a7adced4b0b87c5
Brendan Tierney <tiernebre@gmail.com>
2021-06-14 23:11:40 -0600

    live sessions test

--- packages/back-end/src/live-session/index.ts
+++ packages/back-end/src/live-session/index.ts
@@ -2,3 +2,4 @@ export * from "./live-session";
 export * from "./live-session-payload";
 export * from "./live-session-ticket.entity";
 export * from "./live-session.service";
+export * from "./live-session.router";

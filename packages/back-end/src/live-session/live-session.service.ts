6bdb035ee4745c73e442391bea3f6b417ffbe62d
Brendan Tierney <tiernebre@gmail.com>
2021-06-14 22:41:23 -0600

    redeem one test

--- packages/back-end/src/live-session/live-session.service.ts
+++ packages/back-end/src/live-session/live-session.service.ts
@@ -2,6 +2,8 @@ import { Repository } from "typeorm";
 import { LiveSessionTicketEntity } from "./live-session-ticket.entity";
 import { SessionPayload } from "../session";
 import { LiveSession } from "./live-session";
+import { LiveSessionPayload } from "./live-session-payload";
+import { UnauthorizedError } from "../error";
 
 export class LiveSessionService {
   constructor(
@@ -18,5 +20,15 @@ export class LiveSessionService {
     return {
       ticket
     };
+  }
+
+  async redeemOne(ticket: string): Promise<LiveSessionPayload> {
+    const foundTicket = await this.liveSessionTicketRepository.findOne({ token: ticket, redeemed: false })
+    if (!foundTicket) {
+      throw new UnauthorizedError("You are not permitted to connect to the live session due to invalid authentication")
+    }
+    return {
+      userId: foundTicket.userId
+    }
   }
 }

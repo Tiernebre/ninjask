d024829abcb42507e6dffb70851ef4e0803e6999
Brendan Tierney <tiernebre@gmail.com>
2021-06-14 22:22:24 -0600

    formatting fixX

--- packages/back-end/src/db/migrations/1623730646753-WebSocketSessionTicketSchema.ts
+++ packages/back-end/src/db/migrations/1623730646753-WebSocketSessionTicketSchema.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class WebSocketSessionTicketSchema1623730646753 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+export class WebSocketSessionTicketSchema1623730646753
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS live_session_ticket (
                 token UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
                 redeemed BOOLEAN NOT NULL DEFAULT false,
@@ -11,11 +12,10 @@ export class WebSocketSessionTicketSchema1623730646753 implements MigrationInter
                 user_id INTEGER NOT NULL REFERENCES users (id)
             );
             CREATE INDEX live_session_ticket_redeemed_idx ON live_session_ticket (redeemed);
-        `)
-    }
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(``)
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(``);
+  }
 }

c670dc6c87853c5b8186bc441c72a09e8362befd
Brendan Tierney <tiernebre@gmail.com>
2021-07-09 20:49:15 -0600

    remove challenge views

--- packages/front-end/src/views/AuthenticatedRoutes.tsx
+++ packages/front-end/src/views/AuthenticatedRoutes.tsx
@@ -2,7 +2,6 @@ import { Fragment, useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import { FetchHttpClient, HttpClient } from "../api/http";
 import { HttpSessionService, SessionPayload } from "../api/session";
-import { ChallengeView } from "./challenge/ChallengeView";
 import { Home } from "./Home";
 
 type AuthenticatedRoutesProps = {
@@ -39,12 +38,6 @@ export const AuthenticatedRoutes = ({
     <Fragment>
       <Route path={homeRoutes} exact>
         <Home httpClient={authedHttpClient} />
-      </Route>
-      <Route path="/challenges/:id" exact>
-        <ChallengeView
-          httpClient={authedHttpClient}
-          sessionPayload={sessionPayload}
-        />
       </Route>
     </Fragment>
   ) : null;

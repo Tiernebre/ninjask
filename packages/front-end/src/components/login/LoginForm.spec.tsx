e99b8d1d196f163bd0d186e0d832e7b339218823
Brendan Tierney <tiernebre@gmail.com>
2021-07-09 21:26:42 -0600

    fix tests

--- packages/front-end/src/components/login/LoginForm.spec.tsx
+++ packages/front-end/src/components/login/LoginForm.spec.tsx
@@ -1,7 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import { LoginForm } from "./LoginForm";
-import { act } from "react-dom/test-utils";
 
 const getAccessKeyInput = () =>
   screen.getByRole("textbox", { name: /Access Key/i });
@@ -11,42 +10,39 @@ const getAccessKeyErrorMessage = () =>
   screen.getByText("Access Key is required.");
 const getPasswordErrorMessage = () => screen.getByText("Password is required.");
 
-it("renders an error message and marks the access key input invalid if it is not filled out", () => {
+it("renders an error message and marks the access key input invalid if it is not filled out", async () => {
   const onSubmit = jest.fn();
   render(<LoginForm onSubmit={onSubmit} loading={false} />);
-  act(() => {
-    user.type(getPasswordInput(), "p@55w0rd");
-    user.click(getSubmitButton());
-  });
+  user.type(getPasswordInput(), "p@55w0rd");
+  user.click(getSubmitButton());
+  await waitFor(() => getAccessKeyErrorMessage())
   expect(getAccessKeyInput()).toBeInvalid();
   expect(getAccessKeyInput()).toHaveClass("is-danger");
   expect(getAccessKeyErrorMessage()).toBeInTheDocument();
   expect(onSubmit).toHaveBeenCalledTimes(0);
 });
 
-it("renders an error message and marks the password input invalid if it is not filled out", () => {
+it("renders an error message and marks the password input invalid if it is not filled out", async () => {
   const onSubmit = jest.fn();
   render(<LoginForm onSubmit={onSubmit} loading={false} />);
-  act(() => {
-    user.type(getAccessKeyInput(), "some-access-key-value");
-    user.click(getSubmitButton());
-  });
+  user.type(getAccessKeyInput(), "some-access-key-value");
+  user.click(getSubmitButton());
+  await waitFor(() => getPasswordErrorMessage())
   expect(getPasswordInput()).toBeInvalid();
   expect(getPasswordInput()).toHaveClass("is-danger");
   expect(getPasswordErrorMessage()).toBeInTheDocument();
   expect(onSubmit).toHaveBeenCalledTimes(0);
 });
 
-it("submits the form when the form is filled out and valid and the user clicks on the login button", () => {
+it("submits the form when the form is filled out and valid and the user clicks on the login button", async () => {
   const onSubmit = jest.fn();
   render(<LoginForm onSubmit={onSubmit} loading={false} />);
   const accessKey = "some-access-key-value";
   const password = "p@55w0rd";
-  act(() => {
-    user.type(getAccessKeyInput(), accessKey);
-    user.type(getPasswordInput(), password);
-    user.click(getSubmitButton());
-  });
+  user.type(getAccessKeyInput(), accessKey);
+  user.type(getPasswordInput(), password);
+  user.click(getSubmitButton());
+  await waitFor(() => expect(onSubmit).toHaveBeenCalledTimes(1))
   expect(onSubmit).toHaveBeenCalledTimes(1);
   expect(onSubmit).toHaveBeenCalledWith({
     accessKey,

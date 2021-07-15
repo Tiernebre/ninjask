3dd75bfe257409eac28814dd2a99966d63581077
Brendan Tierney <tiernebre@gmail.com>
2021-07-14 19:30:13 -0600

    more cleanup

--- packages/front-end/src/api/challenge/index.ts
+++ packages/front-end/src/api/challenge/index.ts
@@ -1,3 +1,6 @@
 export * from "./Challenge";
+export * from "./ChallengeResult";
 export * from "./ChallengeService";
 export * from "./HttpChallengeService";
+export * from "./ChallengeParticipantService";
+export * from "./HttpChallengeParticipantService";

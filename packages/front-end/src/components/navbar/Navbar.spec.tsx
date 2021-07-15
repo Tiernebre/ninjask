b71d8be895b3551a6bdeb1243d99f7da344f70d9
Brendan Tierney <tiernebre@gmail.com>
2021-07-14 19:19:08 -0600

    clean up tests

--- packages/front-end/src/components/navbar/Navbar.spec.tsx
+++ packages/front-end/src/components/navbar/Navbar.spec.tsx
@@ -3,8 +3,6 @@ import { Navbar } from "./Navbar";
 import user from "@testing-library/user-event";
 import { MemoryRouter } from "react-router";
 
-const getMenu = () => screen.getByRole("menu");
-
 it("logs the user out if they are logged in", async () => {
   const onLogOut = jest.fn();
   render(
@@ -25,26 +23,4 @@ it("does not show the logout button if the user is not logged in", () => {
   );
   const logoutButton = screen.queryByRole("button", { name: /Log Out/g });
   expect(logoutButton).toBeNull();
-});
-
-it("hides the menu by default", () => {
-  render(
-    <MemoryRouter>
-      <Navbar isAuthenticated={false} onLogOut={jest.fn()} />
-    </MemoryRouter>
-  );
-  const menu = getMenu();
-  expect(menu).not.toHaveClass("is-active");
-});
-
-it("shows the menu when the open menu button is clicked", () => {
-  render(
-    <MemoryRouter>
-      <Navbar isAuthenticated={false} onLogOut={jest.fn()} />
-    </MemoryRouter>
-  );
-  const openMenuButton = screen.getByRole("button", { name: "Open Menu" });
-  user.click(openMenuButton);
-  const menu = getMenu();
-  expect(menu).toHaveClass("is-active");
 });

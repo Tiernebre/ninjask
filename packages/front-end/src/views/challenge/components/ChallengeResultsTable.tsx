1158b20c0e33da2e0cfc9dc3208e0e262561e305
Brendan Tierney <tiernebre@gmail.com>
2021-07-15 19:39:40 -0600

    initial challenge results table

---
+++ packages/front-end/src/views/challenge/components/ChallengeResultsTable.tsx
@@ -0,0 +1,46 @@
+import { Table } from "@tiernebre/kecleon";
+import { ChallengeResult } from "../../../api";
+
+const formatCompletionTimeForResult = (result: ChallengeResult): string => {
+  const { completionTimeHour: hour, completionTimeMinutes: minutes } = result;
+  return hour && minutes ? `${hour}:${minutes}` : "Not Completed Yet";
+};
+
+export type ChallengeResultsTableRowProps = {
+  result: ChallengeResult;
+};
+
+const ChallengeResultsTableRow = ({
+  result,
+}: ChallengeResultsTableRowProps): JSX.Element => {
+  return (
+    <tr>
+      <td>{result.nickname}</td>
+      <td>{formatCompletionTimeForResult(result)}</td>
+    </tr>
+  );
+};
+
+type ChallengeResultsTableProps = {
+  results: ChallengeResult[];
+};
+
+export const ChallengeResultsTable = ({
+  results,
+}: ChallengeResultsTableProps): JSX.Element => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Completion Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((result) => (
+          <ChallengeResultsTableRow key={result.resultId} result={result} />
+        ))}
+      </tbody>
+    </Table>
+  );
+};

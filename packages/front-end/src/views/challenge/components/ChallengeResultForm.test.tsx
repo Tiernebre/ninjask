38db1167352be621fd99abcab7c41aacdfccb627
Brendan Tierney <tiernebre@gmail.com>
2021-07-16 21:12:18 -0600

    test hour max

--- packages/front-end/src/views/challenge/components/ChallengeResultForm.test.tsx
+++ packages/front-end/src/views/challenge/components/ChallengeResultForm.test.tsx
@@ -14,3 +14,19 @@ it("displays an error message if the hour field is not filled out", async () => 
   expect(getHourInput()).toBeInvalid();
   expect(onSubmit).not.toHaveBeenCalled();
 });
+
+it.each([-1000, -1, 100, Number.MAX_SAFE_INTEGER])(
+  "displays an error message that hour needs to be within a range when %p is provided as the hour",
+  async (hourToInput: number) => {
+    const onSubmit = jest.fn();
+    render(<ChallengeResultForm onSubmit={onSubmit} />);
+    user.type(getHourInput(), hourToInput.toString());
+    user.click(getSubmitButton());
+    const hourErrorMessage = await screen.findByText(
+      "Hour must be between 0-99."
+    );
+    expect(hourErrorMessage).toBeInTheDocument();
+    expect(getHourInput()).toBeInvalid();
+    expect(onSubmit).not.toHaveBeenCalled();
+  }
+);

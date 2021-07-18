f2cb3364853d9f806290be998f0a2dacc421a0b6
Brendan Tierney <tiernebre@gmail.com>
2021-07-17 19:30:37 -0600

    uuid test

--- packages/front-end/src/hooks/session/use-sessions.test.tsx
+++ packages/front-end/src/hooks/session/use-sessions.test.tsx
@@ -1,5 +1,7 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { Session } from "../../api";
 import { SessionProvider, useSession } from "./use-session";
+import { v4 as uuid } from "uuid";
 
 it("has the correct initial values", () => {
   const { result } = renderHook(() => useSession(), {
@@ -8,4 +10,18 @@ it("has the correct initial values", () => {
   expect(result.current.session).toBeUndefined();
   expect(result.current.accessToken).toBeUndefined();
   expect(result.current.setSession).toBeTruthy();
+});
+
+it("can update the session", () => {
+  const { result } = renderHook(() => useSession(), {
+    wrapper: SessionProvider,
+  });
+  const session: Session = {
+    accessToken: uuid(),
+    accessTokenExpiration: 0,
+  };
+  act(() => {
+    result.current.setSession(session);
+  });
+  expect(result.current.session).toEqual(session);
 });

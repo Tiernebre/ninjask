451a0bfb63484e07280415d898a549d23c5eb956
Brendan Tierney <tiernebre@gmail.com>
2021-07-17 22:01:49 -0600

    working login test

--- packages/front-end/src/views/login/Login.spec.tsx
+++ packages/front-end/src/views/login/Login.spec.tsx
@@ -1,10 +1,13 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import { Login } from "./Login";
-import { matchers, object, verify, when } from "testdouble";
-import { SessionService } from "../../api";
+import { matchers, verify, when } from "testdouble";
 import { MemoryRouter, Route, Switch } from "react-router";
 import { AlertsProvider } from "@tiernebre/kecleon";
+import {
+  generateMockSessionContext,
+  MockSessionContextProvider,
+} from "../../test/hooks";
 
 const getAccessKeyInput = () => screen.getByLabelText(/Access Key/i);
 const getPasswordInput = () => screen.getByLabelText(/Password/i);
@@ -13,36 +16,37 @@ const getSubmitButton = () => screen.getByRole("button", { name: /Login/i });
 it("processes a fully valid login", async () => {
   const accessKey = "access-key";
   const password = "p@55w0rd";
-  const sessionService = object<SessionService>();
   const accessToken = "access-token";
   const accessTokenExpiration = 10000;
-  const onSuccess = jest.fn();
-  when(sessionService.createOne({ accessKey, password })).thenResolve({
+  const context = generateMockSessionContext();
+  const session = {
     accessToken,
     accessTokenExpiration,
-  });
+  };
+  when(context.sessionService.createOne({ accessKey, password })).thenResolve(
+    session
+  );
   const expectedHomeMessage = "Hey! The Home Route Loaded Up Properly!";
   render(
-    <AlertsProvider>
-      <MemoryRouter>
-        <Login onSuccess={onSuccess} sessionService={sessionService} />
-        <Switch>
-          <Route path="/home" exact>
-            {expectedHomeMessage}
-          </Route>
-        </Switch>
-      </MemoryRouter>
-    </AlertsProvider>
+    <MockSessionContextProvider value={context}>
+      <AlertsProvider>
+        <MemoryRouter>
+          <Login />
+          <Switch>
+            <Route path="/home" exact>
+              {expectedHomeMessage}
+            </Route>
+          </Switch>
+        </MemoryRouter>
+      </AlertsProvider>
+    </MockSessionContextProvider>
   );
   user.type(getAccessKeyInput(), accessKey);
   user.type(getPasswordInput(), password);
   user.click(getSubmitButton());
   await waitFor(() => {
-    verify(sessionService.createOne(matchers.anything()));
+    verify(context.sessionService.createOne(matchers.anything()));
   });
-  expect(onSuccess).toHaveBeenCalledWith({
-    accessToken,
-    accessTokenExpiration,
-  });
+  expect(context.setSession).toHaveBeenCalledWith(session);
   expect(screen.getByText(expectedHomeMessage)).toBeInTheDocument();
 });

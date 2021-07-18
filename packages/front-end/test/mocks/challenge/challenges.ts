63752eac45dc42db2e78e936ff98e685a2690eaa
Brendan Tierney <tiernebre@gmail.com>
2021-07-18 00:50:11 -0600

    get challenges setup

--- packages/front-end/test/mocks/challenge/challenges.ts
+++ packages/front-end/test/mocks/challenge/challenges.ts
@@ -4,7 +4,8 @@ import { generateMockChallenge } from "./generate";
 const createChallenges = (): Record<number, Challenge> => {
   const challenges = {};
   for (let i = 0; i < 10; i++) {
-    challenges[i] = generateMockChallenge();
+    const challenge = generateMockChallenge();
+    challenges[challenge.id] = challenge;
   }
   return challenges;
 };

2a35a83e35cf233c8e762f4ff24ee99055f3c1b7
Brendan Tierney <tiernebre@gmail.com>
2021-07-19 21:38:32 -0600

    expiration setup

--- packages/front-end/test/mocks/session/generate.ts
+++ packages/front-end/test/mocks/session/generate.ts
@@ -1,7 +1,29 @@
 import { Session } from "../../../src/api";
 import { v4 as uuid } from "uuid";
+import jwt from "jsonwebtoken";
+import { generateRandomNumber } from "../../random/random";
+
+const generateAccessToken = (): string => {
+  return jwt.sign(
+    {
+      userId: generateRandomNumber(),
+      accessKey: uuid(),
+      userFingerprint: uuid(),
+    },
+    "Test_Secret_Shhhhh",
+    {
+      expiresIn: "10 minutes",
+    }
+  );
+};
+
+const generateAccessTokenExpiration = (): number => {
+  const date = new Date();
+  date.setMinutes(date.getMinutes() + 20);
+  return date.getTime() / 1000;
+};
 
 export const generateMockSession = (): Session => ({
-  accessToken: uuid(),
-  accessTokenExpiration: new Date().getMilliseconds(),
+  accessToken: generateAccessToken(),
+  accessTokenExpiration: generateAccessTokenExpiration(),
 });

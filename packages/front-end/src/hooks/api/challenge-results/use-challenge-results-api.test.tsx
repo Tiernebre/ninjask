24fc16520d5a3ad31225205e7857c666f8de17be
Brendan Tierney <tiernebre@gmail.com>
2021-07-19 22:16:53 -0600

    challenge result handler

--- packages/front-end/src/hooks/api/challenge-results/use-challenge-results-api.test.tsx
+++ packages/front-end/src/hooks/api/challenge-results/use-challenge-results-api.test.tsx
@@ -1,25 +1,28 @@
 import { renderHook, act } from "@testing-library/react-hooks";
+import { AlertsProvider } from "@tiernebre/kecleon";
 import { PropsWithChildren } from "react";
 import { ISessionContext } from "../..";
 import {
   MockSessionContextProvider,
   generateMockSessionContext,
 } from "../../../../test";
-import { challenges } from "../../../../test/mocks";
+import { challengeResults, challenges } from "../../../../test/mocks";
 import { useChallengeResultsApi } from "./use-challenge-results-api";
 
 const wrapper =
   (value: ISessionContext) =>
   ({ children }: PropsWithChildren<unknown>): JSX.Element =>
     (
-      <MockSessionContextProvider value={value}>
-        {children}
-      </MockSessionContextProvider>
+      <AlertsProvider>
+        <MockSessionContextProvider value={value}>
+          {children}
+        </MockSessionContextProvider>
+      </AlertsProvider>
     );
 
 it("fetches a challenge", async () => {
   const challengeId = Number(Object.keys(challenges)[0]);
-  const expectedChallenge = challenges[challengeId];
+  const expectedChallengeResults = challengeResults[challengeId];
   const { result } = renderHook(() => useChallengeResultsApi({ challengeId }), {
     wrapper: wrapper(generateMockSessionContext()),
   });
@@ -27,5 +30,5 @@ it("fetches a challenge", async () => {
   await act(async () => {
     await result.current.fetchChallengeResults();
   });
-  expect(result.current.results).toEqual(expectedChallenge);
+  expect(result.current.results).toEqual(expectedChallengeResults);
 });

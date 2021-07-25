62091918cae851182cc31e544bb166115ac5e545
Brendan Tierney <tiernebre@gmail.com>
2021-07-24 23:11:18 -0600

    challenges http operation setup:

--- packages/front-end/src/hooks/api/challenges/use-challenges-api.ts
+++ packages/front-end/src/hooks/api/challenges/use-challenges-api.ts
@@ -1,10 +1,20 @@
-import { Challenge } from "../../../api";
+import { Challenge, HttpChallengeService } from "../../../api";
+import { useHttp } from "../../";
+import { useState, useMemo } from "react";
 
 export type UseGetChallengesReturnValue = {
   challenges: Challenge[];
 };
 
 export const useGetChallengesApi = (): UseGetChallengesReturnValue => {
+  const { httpClient } = useHttp();
+  const [challenges, setChallenges] = useState<Challenge[]>([]);
+
+  const challengeService = useMemo(
+    () => new HttpChallengeService(httpClient),
+    [httpClient]
+  );
+
   return {
     challenges: [],
   };

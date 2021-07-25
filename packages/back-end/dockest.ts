1b274ed26d187caa8fa8ac35ea488504ab8121d7
Brendan Tierney <tiernebre@gmail.com>
2021-07-25 12:06:46 -0600

    only run int tests config

--- packages/back-end/dockest.ts
+++ packages/back-end/dockest.ts
@@ -1,7 +1,17 @@
 import { Dockest } from "dockest";
 
+const onlyRunIntegrationTests = process.argv[2] === "--onlyRunIntegrationTests";
+
+const jestOpts = onlyRunIntegrationTests
+  ? {
+      testMatch: ["**/?(*.int.)+(spec|test).[jt]s?(x)"],
+      watch: true,
+    }
+  : undefined;
+
 const dockest = new Dockest({
   composeFile: "docker-compose-int.yml",
+  jestOpts,
 });
 
 const dockestServices = [

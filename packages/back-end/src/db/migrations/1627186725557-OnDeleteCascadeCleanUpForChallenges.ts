dc91b283dfd76ff04591224f6e2d382945898930
Brendan Tierney <tiernebre@gmail.com>
2021-07-24 22:19:28 -0600

    do it on participants side

--- packages/back-end/src/db/migrations/1627186725557-OnDeleteCascadeCleanUpForChallenges.ts
+++ packages/back-end/src/db/migrations/1627186725557-OnDeleteCascadeCleanUpForChallenges.ts
@@ -3,7 +3,20 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class OnDeleteCascadeCleanUpForChallenges1627186725557
   implements MigrationInterface
 {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    ALTER TABLE challenge_participants
+    DROP CONSTRAINT challenge_users_users_challenge_id_fkey,
+    ADD CONSTRAINT challenge_users_users_challenge_id_fkey
+      FOREIGN KEY (challenge_id)
+      REFERENCES challenge (id)
+      ON DELETE CASCADE;
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+
+  `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("");
+  }
 }

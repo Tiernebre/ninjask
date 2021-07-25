dcd0ce4b61a0bb7c8e400dda79667b72b1beab6c
Brendan Tierney <tiernebre@gmail.com>
2021-07-24 22:43:44 -0600

    challenges

---
+++ packages/front-end/src/views/challenges/ChallengesView.tsx
@@ -0,0 +1,3 @@
+export const ChallengesView = (): JSX.Element => {
+  return <div>Challenges w00t!</div>;
+};
--- packages/front-end/src/views/index.ts
+++ packages/front-end/src/views/index.ts
@@ -1,4 +1,5 @@
 export * from "./challenge";
+export * from "./challenges";
 export * from "./home";
 export * from "./login";
 export * from "./routes";

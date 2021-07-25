d452fef59b5416438dc179afa618d684efa2bb48
Brendan Tierney <tiernebre@gmail.com>
2021-07-24 23:48:09 -0600

    heading group setup

--- packages/front-end/src/views/challenges/ChallengesView.tsx
+++ packages/front-end/src/views/challenges/ChallengesView.tsx
@@ -1,12 +1,21 @@
-import { Container, useDidMount } from "@tiernebre/kecleon";
+import { Container, HeadingGroup, useDidMount } from "@tiernebre/kecleon";
+import { ChallengeTable } from "../../components";
 import { useGetChallengesApi } from "../../hooks";
 
 export const ChallengesView = (): JSX.Element => {
   const { challenges, fetchChallenges } = useGetChallengesApi();
 
+  useDidMount(() => {
+    void fetchChallenges();
+  });
+
   return (
     <Container>
-      <div>Challenges w00t!</div>
+      <HeadingGroup
+        title="Challenges"
+        subtitle="View and join challenges from other leagues"
+      />
+      <ChallengeTable challenges={challenges} />
     </Container>
   );
 };

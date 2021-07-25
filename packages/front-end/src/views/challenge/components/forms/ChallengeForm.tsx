e209174b009a832514c355705638e370c6bf355e
Brendan Tierney <tiernebre@gmail.com>
2021-07-25 14:43:19 -0600

    on submit setup

--- packages/front-end/src/views/challenge/components/forms/ChallengeForm.tsx
+++ packages/front-end/src/views/challenge/components/forms/ChallengeForm.tsx
@@ -1,9 +1,22 @@
-export const ChallengeForm = (): JSX.Element => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormData>();
+import { useForm } from "react-hook-form";
+import {
+  CreateChallengeRequest,
+  createChallengeRequestSchema,
+} from "../../../../api";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-  return <form></form>;
+export type ChallengeFormProps = {
+  onSubmit: (request: CreateChallengeRequest) => void;
+};
+
+export const ChallengeForm = ({
+  onSubmit,
+}: ChallengeFormProps): JSX.Element => {
+  const { handleSubmit } = useForm<CreateChallengeRequest>({
+    resolver: zodResolver(createChallengeRequestSchema),
+  });
+
+  const submit = handleSubmit((data) => onSubmit(data));
+
+  return <form onSubmit={submit}></form>;
 };

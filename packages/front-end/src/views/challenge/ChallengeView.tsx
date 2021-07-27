6ec3604cd9a7a82cf44c35e74e315041ea53487b
Brendan Tierney <tiernebre@gmail.com>
2021-07-26 22:14:18 -0600

    page spinner

--- packages/front-end/src/views/challenge/ChallengeView.tsx
+++ packages/front-end/src/views/challenge/ChallengeView.tsx
@@ -25,39 +25,39 @@ export const ChallengeView = (): JSX.Element | null => {
 
   const participantsColumnSize = userIsInChallenge ? 8 : 12;
 
-  // if (challenge && results) {
-  //   return <PageSpinner size="large" />
-  // }
-
-  return challenge && results ? (
-    <Container as="section">
-      <ChallengeViewHeader
-        challenge={challenge}
-        inChallenge={userIsInChallenge}
-        ownsChallenge={userOwnsChallenge}
-        onLeaveChallenge={removeUserFromChallenge}
-        onJoinChallenge={addUserToChallenge}
-        onDeleteChallenge={deleteChallenge}
-      />
-      <Columns>
-        <Column size={participantsColumnSize}>
-          <Box>
-            <Title level={4}>Participants</Title>
-            <ChallengeResultsTable results={results} />
-          </Box>
-        </Column>
-        {userIsInChallenge && (
-          <Column size={4}>
+  if (challenge && results) {
+    return (
+      <Container as="section">
+        <ChallengeViewHeader
+          challenge={challenge}
+          inChallenge={userIsInChallenge}
+          ownsChallenge={userOwnsChallenge}
+          onLeaveChallenge={removeUserFromChallenge}
+          onJoinChallenge={addUserToChallenge}
+          onDeleteChallenge={deleteChallenge}
+        />
+        <Columns>
+          <Column size={participantsColumnSize}>
             <Box>
-              <Title level={4}>Submit Your Result</Title>
-              <ChallengeResultForm
-                onSubmit={submitResult}
-                existingResult={existingResultForUser}
-              />
+              <Title level={4}>Participants</Title>
+              <ChallengeResultsTable results={results} />
             </Box>
           </Column>
-        )}
-      </Columns>
-    </Container>
-  ) : null;
+          {userIsInChallenge && (
+            <Column size={4}>
+              <Box>
+                <Title level={4}>Submit Your Result</Title>
+                <ChallengeResultForm
+                  onSubmit={submitResult}
+                  existingResult={existingResultForUser}
+                />
+              </Box>
+            </Column>
+          )}
+        </Columns>
+      </Container>
+    );
+  } else {
+    return <PageSpinner />;
+  }
 };

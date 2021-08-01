2b845b6fd4e299bedd612f41022d212268f4ecb8
Brendan Tierney <tiernebre@gmail.com>
2021-08-01 16:25:54 -0600

    linter feedback

--- packages/back-end/src/version/version.entity.ts
+++ packages/back-end/src/version/version.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  OneToMany,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { VersionDeniedPokemonEntity } from "./version-denied-pokemon.entity";
 
 @Entity({ name: "versions" })

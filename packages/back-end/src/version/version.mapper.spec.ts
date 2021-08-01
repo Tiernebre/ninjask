bb591fa7551aeeca9c54069306b446234ae166fd
Brendan Tierney <tiernebre@gmail.com>
2021-08-01 16:14:49 -0600

    version mapping tests

--- packages/back-end/src/version/version.mapper.spec.ts
+++ packages/back-end/src/version/version.mapper.spec.ts
@@ -9,7 +9,10 @@ import {
   mapVersionFromPokeApi,
   mapVersionGroupFromPokeApi,
 } from "./version.mapper";
-import { generateMockVersionEntity } from "./version.mock";
+import {
+  generateMockVersionDeniedPokemon,
+  generateMockVersionEntity,
+} from "./version.mock";
 
 describe("version.mapper", () => {
   describe("mapPokedexFromPokeApi", () => {
@@ -67,6 +70,22 @@ describe("version.mapper", () => {
       expect(mappedVersion.id).toEqual(entity.id);
       expect(mappedVersion.name).toEqual(entity.name);
       expect(mappedVersion.versionGroupUrl).toEqual(entity.versionGroupUrl);
+    });
+
+    it("returns a properly formatted version DTO with a denied pokemon ID list", () => {
+      const entity = generateMockVersionEntity();
+      entity.deniedPokemon = [
+        generateMockVersionDeniedPokemon(),
+        generateMockVersionDeniedPokemon(),
+      ];
+      const deniedPokemonIds = entity.deniedPokemon.map(
+        ({ pokemonId }) => pokemonId
+      );
+      const mappedVersion = mapVersionFromEntity(entity);
+      expect(mappedVersion.id).toEqual(entity.id);
+      expect(mappedVersion.name).toEqual(entity.name);
+      expect(mappedVersion.versionGroupUrl).toEqual(entity.versionGroupUrl);
+      expect(mappedVersion.deniedPokemonIds).toEqual(new Set(deniedPokemonIds));
     });
   });
 });

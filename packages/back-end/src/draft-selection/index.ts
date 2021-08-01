c7d48bffda0a22304201d8f29b069dbf174b2819
Brendan Tierney <tiernebre@gmail.com>
2021-08-01 16:52:34 -0600

    package cleanup

--- packages/back-end/src/draft-selection/index.ts
+++ packages/back-end/src/draft-selection/index.ts
@@ -3,3 +3,4 @@ export * from "./draft-selection.entity";
 export * from "./draft-selection.service";
 export * from "./draft-selection.router";
 export * from "./finalize-draft-selection-request";
+export * from "./live-draft-selection.middleware";

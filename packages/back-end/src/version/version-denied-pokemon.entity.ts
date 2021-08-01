6a464b85d56ccb27617f457970141e4b4bd92623
Brendan Tierney <tiernebre@gmail.com>
2021-08-01 16:41:29 -0600

    entity setup

--- packages/back-end/src/version/version-denied-pokemon.entity.ts
+++ packages/back-end/src/version/version-denied-pokemon.entity.ts
@@ -1,21 +1,21 @@
 import {
   Column,
   Entity,
+  JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
-  Unique,
 } from "typeorm";
 import { VersionEntity } from "./version.entity";
 
 @Entity({
   name: "version_denied_pokemon",
 })
-@Unique(["versionId", "pokemonId"])
 export class VersionDeniedPokemonEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
   @ManyToOne(() => VersionEntity, (version) => version.deniedPokemon)
+  @JoinColumn({ name: "version_id" })
   version!: VersionEntity;
 
   @Column()

72f6a94767291edc2d60ba7d661e8c4e40f47545
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 21:30:18 -0600

    leagues fetch

--- packages/front-end/src/views/leagues/LeaguesView.tsx
+++ packages/front-end/src/views/leagues/LeaguesView.tsx
@@ -1,10 +1,18 @@
 import { Container } from "@tiernebre/kecleon";
+import { useEffect } from "react";
 import { LeagueTable } from "../../components";
+import { useGetLeagues } from "../../hooks/api/leagues/use-get-leagues";
 
 export const LeaguesView = (): JSX.Element => {
+  const { leagues, fetchLeagues } = useGetLeagues();
+
+  useEffect(() => {
+    void fetchLeagues();
+  }, [fetchLeagues]);
+
   return (
     <Container as="section">
-      <LeagueTable leagues={[]} />
+      <LeagueTable leagues={leagues} />
     </Container>
   );
 };

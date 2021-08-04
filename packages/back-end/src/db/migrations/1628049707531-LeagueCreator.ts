e068fc9aeb944660b6328f6c4c17f0c32379a5f8
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 22:21:33 -0600

    Fix DB Migration for League Creator ID (#298)
    * add default
    
    * drop default
    
    * fix default migration

--- packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
+++ packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
@@ -3,7 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class LeagueCreator1628049707531 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE;
+            -- 1 is the creator ID for Brendan (creator of the web-application). Only 1 league at the time of this migration so this is a safe operation.
+            ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL DEFAULT 1 REFERENCES users (id) ON DELETE CASCADE;
+            -- Now that the defaults have been applied, we're going to remove it since this shouldn't be a default supported column. It only is for migration.
+            ALTER TABLE IF EXISTS league ALTER COLUMN creator_id DROP DEFAULT;
         `);
   }
 

164ebaab35aba9692d21f50a90ed5f06fc56b5d1
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 22:12:35 -0600

    add default

--- packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
+++ packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class LeagueCreator1628049707531 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE;
+            ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL DEFAULT 1 REFERENCES users (id) ON DELETE CASCADE;
         `);
   }
 

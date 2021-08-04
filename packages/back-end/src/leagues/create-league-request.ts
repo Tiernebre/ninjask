b6ae08f1f35c68ddf85a588759da683fac9cf433
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 20:20:26 -0600

    map created league

--- packages/back-end/src/leagues/create-league-request.ts
+++ packages/back-end/src/leagues/create-league-request.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
-export const createLeagueRequestSchema = z.object({
-  name: z.string().min(1).max(32),
-  description: z.string().max(32).optional(),
-});
+export const createLeagueRequestSchema = z
+  .object({
+    name: z.string().min(1).max(32),
+    description: z.string().max(32).optional(),
+  })
+  .strict();
 
 export type CreateLeagueRequest = z.infer<typeof createLeagueRequestSchema>;
--- packages/back-end/src/leagues/league.service.ts
+++ packages/back-end/src/leagues/league.service.ts
@@ -1,5 +1,6 @@
 import { Repository } from "typeorm";
 import { Logger } from "../logger";
+import { CreateLeagueRequest } from "./create-league-request";
 import { League } from "./league";
 import { LeagueEntity } from "./league.entity";
 
@@ -12,11 +13,27 @@ export class LeagueService {
   public async getAll(): Promise<League[]> {
     this.logger.info("Retrieving all of the leagues.");
     const foundLeagues = await this.leagueRepository.find();
-    return foundLeagues.map((league) => ({
-      id: league.id,
-      name: league.name,
-      description: league.description,
-      creatorId: league.creatorId,
-    }));
+    return foundLeagues.map((league) => this.map(league));
+  }
+
+  public async createOne(
+    request: CreateLeagueRequest,
+    creatorId: number
+  ): Promise<League> {
+    const leagueToSave = this.leagueRepository.create({
+      ...request,
+      creatorId,
+    });
+    const createdLeague = await this.leagueRepository.save(leagueToSave);
+    return this.map(createdLeague);
+  }
+
+  private map(entity: LeagueEntity): League {
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      creatorId: entity.creatorId,
+    };
   }
 }

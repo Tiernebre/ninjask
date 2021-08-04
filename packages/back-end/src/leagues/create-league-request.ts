8db137130c76a6f988727a133d2ed2fc46e265a7
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 20:18:20 -0600

    infer DTO

--- packages/back-end/src/leagues/create-league-request.ts
+++ packages/back-end/src/leagues/create-league-request.ts
@@ -4,3 +4,5 @@ export const createLeagueRequestSchema = z.object({
   name: z.string().min(1).max(32),
   description: z.string().max(32).optional(),
 });
+
+export type CreateLeagueRequest = z.infer<typeof createLeagueRequestSchema>;

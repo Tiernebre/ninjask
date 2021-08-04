89acd018809a5c7d7a20143f835c4ff48ff81d1d
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 21:19:38 -0600

    leagues endpoint

--- packages/front-end/test/server/league-handlers.ts
+++ packages/front-end/test/server/league-handlers.ts
@@ -1,1 +1,13 @@
-export const leagueHandlers = [];
+import { rest } from "msw";
+import { leagues } from "../mocks/league";
+import { HOST } from "./constants";
+import { isAuthorized } from "./helpers";
+
+export const leagueHandlers = [
+  rest.get(`${HOST}leagues`, (req, res, ctx) => {
+    if (!isAuthorized(req)) {
+      return res(ctx.status(403), ctx.json({ message: "Not Authorized" }));
+    }
+    return res(ctx.json(Object.values(leagues)));
+  }),
+];

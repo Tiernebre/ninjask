bbf85f61e9ffe6a74a64532936b9d8f452f40765
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 22:16:56 -0600

    fix default migration

--- packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
+++ packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
@@ -6,7 +6,7 @@ export class LeagueCreator1628049707531 implements MigrationInterface {
             -- 1 is the creator ID for Brendan (creator of the web-application). Only 1 league at the time of this migration so this is a safe operation.
             ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL DEFAULT 1 REFERENCES users (id) ON DELETE CASCADE;
             -- Now that the defaults have been applied, we're going to remove it since this shouldn't be a default supported column. It only is for migration.
-            ALTER TABLE IF EXISTS league ALTER COLUMN IF EXISTS creator_id DROP DEFAULT;
+            ALTER TABLE IF EXISTS league ALTER COLUMN creator_id DROP DEFAULT;
         `);
   }
 

e78125d154470539f33479966be193efa41a9c8c
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 22:15:17 -0600

    drop default

--- packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
+++ packages/back-end/src/db/migrations/1628049707531-LeagueCreator.ts
@@ -3,7 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class LeagueCreator1628049707531 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
+            -- 1 is the creator ID for Brendan (creator of the web-application). Only 1 league at the time of this migration so this is a safe operation.
             ALTER TABLE IF EXISTS league ADD COLUMN IF NOT EXISTS creator_id INTEGER NOT NULL DEFAULT 1 REFERENCES users (id) ON DELETE CASCADE;
+            -- Now that the defaults have been applied, we're going to remove it since this shouldn't be a default supported column. It only is for migration.
+            ALTER TABLE IF EXISTS league ALTER COLUMN IF EXISTS creator_id DROP DEFAULT;
         `);
   }
 

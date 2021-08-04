75e67d20662d88f16fded8fbc125c21e6e7ec14f
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 21:13:27 -0600

    fetch leagues API

---
+++ packages/front-end/src/hooks/api/leagues/use-get-leagues.ts
@@ -0,0 +1,22 @@
+import { useCallback, useState } from "react";
+import { League } from "../../../api";
+import { useLeaguesApi } from "./use-leagues-api";
+
+export type GetLeaguesHookReturnValue = {
+  leagues: League[];
+  fetchLeagues: () => Promise<void>;
+};
+
+export const useGetLeagues = (): GetLeaguesHookReturnValue => {
+  const [leagues, setLeagues] = useState<League[]>([]);
+  const { getLeagues } = useLeaguesApi();
+
+  const fetchLeagues = useCallback(async () => {
+    setLeagues(await getLeagues());
+  }, [getLeagues]);
+
+  return {
+    leagues,
+    fetchLeagues,
+  };
+};

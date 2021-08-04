4b75f87dfd4f6bcbf1a5d412b28e79c7ffccf9cc
Brendan Tierney <tiernebre@gmail.com>
2021-08-03 19:40:25 -0600

    league creator id

--- packages/back-end/src/db/migrations/1628040851533-LeagueCreatorId.ts
+++ packages/back-end/src/db/migrations/1628040851533-LeagueCreatorId.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class LeagueCreatorId1628040851533 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE league ADD COLUMN creator_id INTEGER REFERENCES users (id) ON DELETE CASCADE;
+            ALTER TABLE league ADD COLUMN creator_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE;
         `);
   }
 

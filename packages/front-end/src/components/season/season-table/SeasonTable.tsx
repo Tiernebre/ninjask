2a41e426937577442a570b6264fd13b48863981c
Brendan Tierney <tiernebre@gmail.com>
2021-08-04 21:55:50 -0600

    season table

--- packages/front-end/src/components/season/season-table/SeasonTable.tsx
+++ packages/front-end/src/components/season/season-table/SeasonTable.tsx
@@ -1,3 +1,26 @@
-export const SeasonTable = (): JSX.Element => {
-  return <div>Season Table</div>;
+import { Table } from "@tiernebre/kecleon";
+import { Season } from "../../../api";
+import { SeasonRow } from "./SeasonRow";
+
+type SeasonTableProps = {
+  seasons: Season[];
+};
+
+export const SeasonTable = ({ seasons }: SeasonTableProps): JSX.Element => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Description</td>
+        </tr>
+      </thead>
+      <tbody>
+        {seasons.map((season) => (
+          <SeasonRow key={season.id} season={season} />
+        ))}
+      </tbody>
+    </Table>
+  );
 };

ef31bbb37da4656a37b44c7c3e493fa4e3c0a0be
Brendan Tierney <tiernebre@gmail.com>
2021-08-04 21:47:23 -0600

    individual league view

---
+++ packages/front-end/src/views/league/LeagueView.tsx
@@ -0,0 +1,22 @@
+import { Container, PageSpinner } from "@tiernebre/kecleon";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useGetLeague } from "../../hooks/api/leagues/use-get-league";
+
+type LeagueViewParams = {
+  id: string;
+};
+
+export const LeagueView = (): JSX.Element => {
+  const { id } = useParams<LeagueViewParams>();
+  const { league, seasons, fetchLeague } = useGetLeague({ id: Number(id) });
+
+  useEffect(() => {
+    void fetchLeague();
+  }, [fetchLeague]);
+
+  const content =
+    league && seasons.length ? <div>League dude</div> : <PageSpinner />;
+
+  return <Container as="section">{content}</Container>;
+};

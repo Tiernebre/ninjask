3910b316d20929f81cd9337fdb759e142d507b85
Brendan Tierney <tiernebre@gmail.com>
2021-08-04 20:55:33 -0600

    get all with league id

--- packages/back-end/src/season/season.repository.int.spec.ts
+++ packages/back-end/src/season/season.repository.int.spec.ts
@@ -2,14 +2,18 @@ import { Repository, getRepository, getCustomRepository } from "typeorm";
 import { establishDbConnection } from "../test/create-db-connection";
 import { SeasonRepository } from "./season.repository";
 import { LeagueEntity } from "../leagues";
-import { seedLeagues } from "../leagues/league.seed";
+import { seedLeague } from "../leagues/league.seed";
 import { seedUser } from "../user/user.seed";
 import { UserEntity } from "../user";
+import { seedSeasons } from "./season.seed";
+import { SeasonEntity } from "./season.entity";
 
 describe("SeasonRepository", () => {
   let seasonRepository: SeasonRepository;
   let leagueRepository: Repository<LeagueEntity>;
   let userRepository: Repository<UserEntity>;
+  let league: LeagueEntity;
+  let seasons: SeasonEntity[];
 
   beforeAll(async () => {
     await establishDbConnection();
@@ -20,6 +24,14 @@ describe("SeasonRepository", () => {
     seasonRepository = getCustomRepository(SeasonRepository);
     userRepository = getRepository(UserEntity);
     const creator = await seedUser(userRepository);
-    await seedLeagues(leagueRepository, creator.id);
+    league = await seedLeague(leagueRepository, creator.id);
+    seasons = await seedSeasons(seasonRepository, league.id);
+  });
+
+  describe("getAllWithLeagueId", () => {
+    it("returns the seasons tied with a given league id", async () => {
+      const foundSeasons = await seasonRepository.getAllWithLeagueId(league.id);
+      expect(foundSeasons).toEqual(seasons);
+    });
   });
 });

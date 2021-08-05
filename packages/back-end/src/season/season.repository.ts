d8c3d90372fb0e780f6fa2cfef275f8ebe93d7e1
Brendan Tierney <tiernebre@gmail.com>
2021-08-04 20:43:00 -0600

    league id

--- packages/back-end/src/season/season.repository.ts
+++ packages/back-end/src/season/season.repository.ts
@@ -2,4 +2,6 @@ import { EntityRepository, Repository } from "typeorm";
 import { SeasonEntity } from "./season.entity";
 
 @EntityRepository(SeasonEntity)
-export class SeasonRepository extends Repository<SeasonEntity> {}
+export class SeasonRepository extends Repository<SeasonEntity> {
+  async getAllWithLeagueId(leagueId: number): Promise<SeasonEntity> {}
+}

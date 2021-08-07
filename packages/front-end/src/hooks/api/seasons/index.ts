5c8ca69cef19c082e649f6e6d922f2a31193368a
Brendan Tierney <tiernebre@gmail.com>
2021-08-06 20:40:56 -0600

    season view

--- packages/front-end/src/hooks/api/seasons/index.ts
+++ packages/front-end/src/hooks/api/seasons/index.ts
@@ -1,2 +1,3 @@
 export * from "./use-get-seasons";
+export * from "./use-get-season";
 export * from "./use-season-api";
--- packages/front-end/src/views/season/SeasonView.tsx
+++ packages/front-end/src/views/season/SeasonView.tsx
@@ -1,3 +1,33 @@
+import { Box, Container, HeadingGroup, PageSpinner } from "@tiernebre/kecleon";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { ChallengeTable } from "../../components";
+import { useGetSeason } from "../../hooks";
+
+type SeasonViewParams = {
+  id: string;
+};
+
 export const SeasonView = (): JSX.Element => {
-  return <div>SeasonView</div>;
+  const { id } = useParams<SeasonViewParams>();
+  const { season, challenges, fetchSeason } = useGetSeason(Number(id));
+
+  useEffect(() => {
+    void fetchSeason();
+  }, [fetchSeason]);
+
+  const content =
+    season && challenges.length ? (
+      <>
+        <HeadingGroup title={season.name} subtitle={season.description} />
+        <Box>
+          <HeadingGroup title="Challenges" level={4} />
+          <ChallengeTable challenges={challenges} />
+        </Box>
+      </>
+    ) : (
+      <PageSpinner />
+    );
+
+  return <Container as="section">{content}</Container>;
 };

a1e94c4018241752ceb0b738a79ff94061c672b8
Brendan Tierney <tiernebre@gmail.com>
2021-08-06 20:03:54 -0600

    challenges on league

--- packages/front-end/src/views/league/LeagueView.tsx
+++ packages/front-end/src/views/league/LeagueView.tsx
@@ -1,6 +1,7 @@
 import { Box, Container, HeadingGroup, PageSpinner } from "@tiernebre/kecleon";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { ChallengeTable } from "../../components";
 import { SeasonTable } from "../../components/season";
 import { useGetLeague } from "../../hooks/api/leagues/use-get-league";
 
@@ -10,19 +11,25 @@ type LeagueViewParams = {
 
 export const LeagueView = (): JSX.Element => {
   const { id } = useParams<LeagueViewParams>();
-  const { league, seasons, fetchLeague } = useGetLeague({ id: Number(id) });
+  const { league, seasons, challenges, fetchLeague } = useGetLeague({
+    id: Number(id),
+  });
 
   useEffect(() => {
     void fetchLeague();
   }, [fetchLeague]);
 
   const content =
-    league && seasons.length ? (
+    league && seasons.length && challenges.length ? (
       <>
         <HeadingGroup title={league.name} subtitle={league.description} />
         <Box>
           <HeadingGroup title="Seasons" level={4} />
           <SeasonTable seasons={seasons} />
+        </Box>
+        <Box>
+          <HeadingGroup title="Challenges" level={4} />
+          <ChallengeTable challenges={challenges} />
         </Box>
       </>
     ) : (

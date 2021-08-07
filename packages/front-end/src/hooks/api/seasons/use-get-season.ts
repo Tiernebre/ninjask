3731b06dfa4723c10388b7bcf525d83fc30d4eb9
Brendan Tierney <tiernebre@gmail.com>
2021-08-06 20:33:34 -0600

    fetch challengess

--- packages/front-end/src/hooks/api/seasons/use-get-season.ts
+++ packages/front-end/src/hooks/api/seasons/use-get-season.ts
@@ -1,23 +1,31 @@
 import { useCallback, useState } from "react";
-import { Season } from "../../../api";
+import { Challenge, Season } from "../../../api";
 import { useSeasonsApi } from "./use-season-api";
 
 type GetSeasonHookReturnValue = {
   season?: Season;
+  challenges: Challenge[];
   fetchSeason: () => Promise<void>;
 };
 
 export const useGetSeason = (id: number): GetSeasonHookReturnValue => {
   const [season, setSeason] = useState<Season>();
+  const [challenges, setChallenges] = useState<Challenge[]>([]);
 
-  const { getSeasonById } = useSeasonsApi();
+  const { getSeasonById, getChallengesForOne } = useSeasonsApi();
 
   const fetchSeason = useCallback(async () => {
-    setSeason(await getSeasonById(id));
-  }, [id, getSeasonById]);
+    const [season, challenges] = await Promise.all([
+      getSeasonById(id),
+      getChallengesForOne(id),
+    ]);
+    setSeason(season);
+    setChallenges(challenges);
+  }, [id, getSeasonById, getChallengesForOne]);
 
   return {
     season,
+    challenges,
     fetchSeason,
   };
 };

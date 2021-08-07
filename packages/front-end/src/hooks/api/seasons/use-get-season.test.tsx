f39abd2c8fd8a2e3a00e7af5b94fef690cfba17c
Brendan Tierney <tiernebre@gmail.com>
2021-08-06 20:34:17 -0600

    challenges setup

--- packages/front-end/src/hooks/api/seasons/use-get-season.test.tsx
+++ packages/front-end/src/hooks/api/seasons/use-get-season.test.tsx
@@ -1,6 +1,10 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import { PropsWithChildren } from "react";
-import { MockSessionContextProvider, seasons } from "../../../../test";
+import {
+  MockSessionContextProvider,
+  seasonChallenges,
+  seasons,
+} from "../../../../test";
 import { useGetSeason } from "./use-get-season";
 
 const wrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => (
@@ -9,12 +13,15 @@ const wrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => (
 
 it("fetches information about a specific season by id and stores it statefully", async () => {
   const [expectedSeason] = Object.values(seasons);
+  const expectedChallenges = seasonChallenges[expectedSeason.id];
   const { result } = renderHook(() => useGetSeason(expectedSeason.id), {
     wrapper,
   });
   expect(result.current.season).toBeUndefined();
+  expect(result.current.challenges).toEqual([]);
   await act(async () => {
     await result.current.fetchSeason();
   });
   expect(result.current.season).toEqual(expectedSeason);
+  expect(result.current.challenges).toEqual(expectedChallenges);
 });

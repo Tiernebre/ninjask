65c1470c4c11c5731ce3550d87ee27c6d009d990
Brendan Tierney <tiernebre@gmail.com>
2021-08-07 23:19:21 -0600

    status

--- packages/front-end/src/api/challenge/index.ts
+++ packages/front-end/src/api/challenge/index.ts
@@ -1,6 +1,7 @@
 export * from "./Challenge";
 export * from "./ChallengeResult";
 export * from "./ChallengeService";
+export * from "./ChallengeStatus";
 export * from "./HttpChallengeService";
 export * from "./ChallengeParticipantService";
 export * from "./HttpChallengeParticipantService";

a8b2ccd7a790ac74e1b9e0886e530e16a6515e94
Brendan Tierney <tiernebre@gmail.com>
2021-08-08 13:01:11 -0600

    create draft request

---
+++ packages/back-end/src/draft/create-draft-request.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const createChallengeRequestSchema = z
+  .object({
+    challengeId: z.number(),
+  })
+  .strict();
+
+export type CreateChallengeRequest = z.infer<
+  typeof createChallengeRequestSchema
+>;

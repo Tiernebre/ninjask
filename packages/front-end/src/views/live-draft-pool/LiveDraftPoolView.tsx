fbd8cfd2b3c3d37021ab1e657bfb527a88da450d
Brendan Tierney <tiernebre@gmail.com>
2021-08-24 20:44:29 -0600

    live draft pool view loading

--- packages/front-end/src/views/live-draft-pool/LiveDraftPoolView.tsx
+++ packages/front-end/src/views/live-draft-pool/LiveDraftPoolView.tsx
@@ -1,71 +1,12 @@
-import { Fragment, useCallback } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
-import { LiveDraftPool, Draft, SessionPayload } from "../../api";
-import { PooledPokemon, PokemonInformation } from "../../components";
+import { Container, PageSpinner } from "@tiernebre/kecleon";
+import { useSessionPayload } from "../../hooks";
 import "./LiveDraftPoolView.scss";
 
-type LiveDraftPoolViewProps = {
-  draft: Draft;
-  challengeOwnerId: number;
-  sessionPayload?: SessionPayload;
-  onFinished: () => void;
-};
 
-export const LiveDraftPoolView = ({
-  draft,
-  sessionPayload,
-  challengeOwnerId,
-  onFinished,
-}: LiveDraftPoolViewProps): JSX.Element => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { lastJsonMessage, readyState, sendMessage } = useWebSocket(
-    `${process.env.REACT_APP_BACK_END_API_WS_URL as string}/drafts/${Number(
-      draft.id
-    )}/live-pool`
-  );
-
-  const getNextPokemon = useCallback(() => {
-    sendMessage("NEXT");
-  }, [sendMessage]);
-
-  const isReady = readyState === ReadyState.OPEN;
-  const currentDraftStatus = lastJsonMessage as LiveDraftPool;
-  const userIsCreator = sessionPayload?.userId === challengeOwnerId;
-
+export const LiveDraftPoolView = (): JSX.Element => {
   return (
-    <Fragment>
-      {isReady && currentDraftStatus ? (
-        <div className="LiveDraftPoolView columns is-marginless">
-          <div className="LiveDraftPoolView__pooled-pokemon-column column is-2 is-12-mobile p-0">
-            <PooledPokemon
-              pokemon={currentDraftStatus.pooledPokemon}
-              poolSize={draft.poolSize}
-            />
-          </div>
-          <div className="LiveDraftPoolView__pokemon-information-column column is-10">
-            <PokemonInformation
-              pokemon={currentDraftStatus.currentPokemon}
-              emptyPlaceholder="The Pool is being loaded..."
-            />
-            <div className="LiveDraftPoolView__button-container mt-5">
-              {userIsCreator && !currentDraftStatus.isPoolOver && (
-                <button className="button" onClick={getNextPokemon}>
-                  Next
-                </button>
-              )}
-              {currentDraftStatus.isPoolOver && (
-                <button className="button is-primary" onClick={onFinished}>
-                  Finish
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="LiveDraftPoolView__loading">
-          <p>Loading Live Draft...</p>
-        </div>
-      )}
-    </Fragment>
-  );
+    <Container>
+      <PageSpinner />
+    </Container>
+  )
 };

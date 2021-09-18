3a956ca5a889e6a519c4cff4ff1deb7da8f4cbaa
Brendan Tierney <tiernebre@gmail.com>
2021-09-18 16:10:34 -0600

    fix up hook

--- packages/front-end/src/views/live-draft/hooks/use-live-draft.ts
+++ packages/front-end/src/views/live-draft/hooks/use-live-draft.ts
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
+import { DraftSelection } from "../../../api";
 import { LiveDraftSelectionMessage } from "../types/live-draft-selection-message";
 
-export const useLiveDraft = (draftId: number): void => {
-  const { lastJsonMessage, readyState, sendMessage } = useWebSocket(
+type LiveDraftHookReturnValue = {
+  currentDraftSelections: DraftSelection[];
+};
+
+export const useLiveDraft = (draftId: number): LiveDraftHookReturnValue => {
+  const [currentDraftSelections, setCurrentDraftSelections] = useState<
+    DraftSelection[]
+  >([]);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { lastMessage, readyState, sendMessage } = useWebSocket(
     `${process.env.REACT_APP_BACK_END_API_WS_URL as string}/drafts/${Number(
       draftId
     )}/live-selections`
   );
 
-  const finalizeSelection = (request: LiveDraftSelectionMessage): void => {
+  const finalizeDraftSelection = (request: LiveDraftSelectionMessage): void => {
     sendMessage(JSON.stringify(request));
+  };
+
+  useEffect(() => {
+    setCurrentDraftSelections(lastMessage as DraftSelection[]);
+  }, [lastMessage]);
+
+  return {
+    currentDraftSelections,
   };
 };

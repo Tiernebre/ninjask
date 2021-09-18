49c2d84919b718f87c793259c5ec08b1b3c8f2ac
Brendan Tierney <tiernebre@gmail.com>
2021-09-18 15:50:11 -0600

    load challenge and draft

--- packages/front-end/src/views/live-draft/LiveDraftView.tsx
+++ packages/front-end/src/views/live-draft/LiveDraftView.tsx
@@ -1,3 +1,27 @@
+import { Container, PageSpinner, useDidMount } from "@tiernebre/kecleon";
+import { useParams } from "react-router-dom";
+import { useGetChallenge } from "../../hooks";
+import { LiveDraft } from "./components/LiveDraft";
+
+type LiveDraftViewParams = {
+  challengeId: string;
+};
+
 export const LiveDraftView = (): JSX.Element => {
-  return <div>Live Draft View</div>;
+  const { challengeId } = useParams<LiveDraftViewParams>();
+  const { challenge, draft, fetchChallenge } = useGetChallenge({
+    challengeId: Number(challengeId),
+  });
+
+  useDidMount(() => {
+    void fetchChallenge();
+  });
+
+  return challenge && draft ? (
+    <LiveDraft />
+  ) : (
+    <Container>
+      <PageSpinner />
+    </Container>
+  );
 };
---
+++ packages/front-end/src/views/live-draft/components/LiveDraft.tsx
@@ -0,0 +1,3 @@
+export const LiveDraft = (): JSX.Element => {
+  return <div>Live Draft</div>;
+};
